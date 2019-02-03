10141faad3eff929cf049bdddd174f15032b0507
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-02-03 10:07:13 -0500

    Update ccc16j3.java

--- CCC Junior/ccc16j3.java
+++ CCC Junior/ccc16j3.java
@@ -5,8 +5,7 @@ public class ccc16j3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		
-		//Hidden Palindrome
+
 		String S = sc.next();
 		int count = 0;
 		
