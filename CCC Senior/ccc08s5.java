379f704f658358925f8ad66adb7a7cf900194cde
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-02-07 09:51:46 -0500

    Update ccc08s5.java

--- CCC Senior/ccc08s5.java
+++ CCC Senior/ccc08s5.java
@@ -1,52 +1,37 @@
-package contest_questions;
-
 import java.util.*;
 
 public class ccc08s5 {
 	
-	static boolean[][][][] win;
-	static boolean[][][][] been;
+	static boolean[][][][] dp, been;
 	
-	public static boolean find(int a, int b, int c, int d){
-		if (a<0||b<0||c<0||d<0){
-			return true;
-		}
-		if (been[a][b][c][d]){
-			return win[a][b][c][d];
-		}
-		been[a][b][c][d] = true;
-		return win[a][b][c][d] =(!find(a-2,b-1,c,d-2)|!find(a-1,b,c,d-1)|!find(a,b-3,c,d)
-				|!find(a-1,b-1,c-1,d-1)|!find(a,b,c-2,d-1));
-	}
+	public static boolean win(int a, int b, int c, int d){
+		//calculate win/loose for the prev
+		if (a<0||b<0||c<0||d<0) return true;
+		else if (been[a][b][c][d]) return dp[a][b][c][d];
+		else {
+			been[a][b][c][d] = true;
 
+			dp[a][b][c][d] = !win(a-2,b-1,c,d-2) 
+					|| !win(a-1,b-1,c-1,d-1)
+					|| !win(a,b,c-2,d-1)
+					|| !win(a,b-3,c,d)
+					|| !win(a-1,b,c,d-1);
+			return dp[a][b][c][d];
+		}
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
 		
-		win = new boolean[31][31][31][31];
+		dp = new boolean[31][31][31][31];
 		been = new boolean[31][31][31][31];
 		
-		//A B C D
-		//2 1 0 2
-		//1 1 1 1
-		//0 0 2 1
-		//0 3 0 0
-		//1 0 0 1
-		
-		
-		//4D array to store
-		
-		int n = sc.nextInt();
-		for (int i=0; i<n; i++){
-			int a = sc.nextInt();
-			int b = sc.nextInt();
-			int c = sc.nextInt();
-			int d = sc.nextInt();
-			if (!find(a,b,c,d)){
-				System.out.println("Roland");
-			} else {
-				System.out.println("Patrick");
-			}
+		for (int i=0; i<t; i++){
+			int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt(), d = sc.nextInt();
+			if (win(a,b,c,d)) System.out.println("Patrick");
+			else System.out.println("Roland");
 		}
 		
 		
