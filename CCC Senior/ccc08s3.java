a2b8835b5e299851b21e4ddbb73a6f6e76f4b8a8
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-02-07 09:48:07 -0500

    Update ccc08s3.java

--- CCC Senior/ccc08s3.java
+++ CCC Senior/ccc08s3.java
@@ -1,126 +1,133 @@
-package contest_questions;
-
-import java.util.Arrays;
-
-import java.util.LinkedList;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class ccc08s3 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		
-		//Solved!
-
-		int t = sc.nextInt();
-		for (int q=0; q<t; q++){
-			LinkedList <Integer> qx = new LinkedList <Integer> ();
-			LinkedList <Integer> qy = new LinkedList <Integer> ();
-
-
-			int r = sc.nextInt();
-			int c = sc.nextInt();
-			int br = 0;
-			int bc = 0;
-			int er = r-1;
-			int ec = c-1;
-
-			qx.add(br);
-			qy.add(bc);
-			int[][] steps = new int[r][c];
-			char[][] a = new char[r][c];
-			sc.nextLine();
-			for (int i=0; i<r; i++){
-				String s = sc.nextLine();
-				for (int l=0; l<s.length(); l++){
-					a[i][l]=s.charAt(l);
-				}
-			}
-			for (int i=0; i<r; i++){
-				Arrays.fill(steps[i], Integer.MAX_VALUE);
-			}
-			steps[0][0] = 0;
-
-
-			while (!qx.isEmpty()){
-				int x;
-				x = qx.poll();
-				int y;
-				y = qy.poll();
-
-				if (a[x][y]=='+'){
-					if (x+1<r&&a[x+1][y]!='*'&&steps[x+1][y]>steps[x][y]+1){
-						steps[x+1][y]=steps[x][y]+1;
-						qx.add(x+1);
-						qy.add(y);
-					}
-
-
-					if (y+1<c&&a[x][y+1]!='*'&&steps[x][y+1]>steps[x][y]+1){
-						steps[x][y+1]=steps[x][y]+1;
-						qx.add(x);
-						qy.add(y+1);
-					}
-
-					if (x-1>=0&&a[x-1][y]!='*'&&steps[x-1][y]>steps[x][y]+1){
-						steps[x-1][y]=steps[x][y]+1;
-						qx.add(x-1);
-						qy.add(y);
-					}
-
-					if (y-1>=0&&a[x][y-1]!='*'&&steps[x][y-1]>steps[x][y]+1){
-						steps[x][y-1]=steps[x][y]+1;
-						qx.add(x);
-						qy.add(y-1);
-					}
-
-
-
-				} else if (a[x][y]=='|'){
-					if (x+1<r&&a[x+1][y]!='*'&&steps[x+1][y]>steps[x][y]+1){
-						steps[x+1][y]=steps[x][y]+1;
-						qx.add(x+1);
-						qy.add(y);
-					}
-
-					if (x-1>=0&&a[x-1][y]!='*'&&steps[x-1][y]>steps[x][y]+1){
-						steps[x-1][y]=steps[x][y]+1;
-						qx.add(x-1);
-						qy.add(y);
-					}
-
-
-				} else if (a[x][y]=='-'){
-					if (y+1<c&&a[x][y+1]!='*'&&steps[x][y+1]>steps[x][y]+1){
-						steps[x][y+1]=steps[x][y]+1;
-						qx.add(x);
-						qy.add(y+1);
-					}
-
-					if (y-1>=0&&a[x][y-1]!='*'&&steps[x][y-1]>steps[x][y]+1){
-						steps[x][y-1]=steps[x][y]+1;
-						qx.add(x);
-						qy.add(y-1);
-					}
-
-
-
-				}
-
-			}
-			
-
-			if (steps[er][ec]+1==Integer.MAX_VALUE || steps[er][ec]+1==Integer.MIN_VALUE){
-				System.out.println(-1);
-			} else{
-				System.out.println(steps[er][ec]+1);
-			}
-
-
+	static class FastReader
+	{
+		BufferedReader br;
+		StringTokenizer st;
+		public FastReader() {
+			br = new BufferedReader(new
+					InputStreamReader(System.in));
 		}
 
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				}
+				catch (IOException  e)
+				{
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
 
+		int nextInt()
+		{
+			return Integer.parseInt(next());
+		}
+
+		long nextLong()
+		{
+			return Long.parseLong(next());
+		}
+
+		double nextDouble()
+		{
+			return Double.parseDouble(next());
+		}
+
+		String nextLine()
+		{
+			String str = "";
+			try
+			{
+				str = br.readLine();
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+			return str;
+		}
 	}
-}
 
+	public static class edge implements Comparable<edge>{
+		int bv, ev, cost, danger;
+		public edge(int b,int e,int c,int d){
+			bv=b;ev=e;cost=c; danger=d;
+		}
+		@Override
+		public int compareTo(edge o) {
+			// TODO Auto-generated method stub
+			if (danger==o.danger){
+				return cost - o.cost;
+			} return danger - o.danger;
+		}
+	}
+	
+	static int[] parent;
+	
+	public static int find(int i){
+		if (parent[i]==-1) return i;
+		else return find(parent[i]);
+	}
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		FastReader sc = new FastReader();
+
+		int t = sc.nextInt();
+		for (int w=0; w<t; w++){
+			int r = sc.nextInt(), c = sc.nextInt();
+			char[][] map = new char[r][c];
+			for (int i=0; i<r; i++){
+				String s = sc.next();
+				map[i] = s.toCharArray();
+			}
+			
+			LinkedList<Integer> X = new LinkedList<Integer>();
+			LinkedList<Integer> Y = new LinkedList<Integer>();
+			
+			int[][] steps = new int[r][c];
+			for (int i=0; i<r; i++){
+				Arrays.fill(steps[i], 1<<30);
+			}
+			steps[0][0] = 1;
+			X.add(0); Y.add(0);
+			while (!X.isEmpty()){
+				int x = X.poll();
+				int y = Y.poll();
+				if (map[x][y]=='-'||map[x][y]=='+'){
+					if (y+1<c&&map[x][y+1]!='*'&&steps[x][y]+1<steps[x][y+1]){
+						steps[x][y+1] = steps[x][y]+1;
+						X.add(x); Y.add(y+1);
+					} if (y-1>=0&&map[x][y-1]!='*'&&steps[x][y]+1<steps[x][y-1]){
+						steps[x][y-1] = steps[x][y]+1;
+						X.add(x); Y.add(y-1);
+					}
+				}
+				if (map[x][y]=='|'||map[x][y]=='+'){
+					if (x+1<r&&map[x+1][y]!='*'&&steps[x][y]+1<steps[x+1][y]){
+						steps[x+1][y] = steps[x][y]+1;
+						X.add(x+1); Y.add(y);
+					} if (x-1>=0&&map[x-1][y]!='*'&&steps[x][y]+1<steps[x-1][y]){
+						steps[x-1][y] = steps[x][y]+1;
+						X.add(x-1); Y.add(y);
+					}
+				}
+			}
+			if (steps[r-1][c-1]==1<<30) System.out.println(-1);
+			else System.out.println(steps[r-1][c-1]);
+			
+			
+			
+		}
+		
+	}
+
+
+}
