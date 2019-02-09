d32204a8416224c94407cf564679e915009db661
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-02-09 10:12:20 -0500

    Update ccc10s5.java

--- CCC Senior/ccc10s5.java
+++ CCC Senior/ccc10s5.java
@@ -1,57 +1,48 @@
-package contest_questions;
-
 import java.util.*;
 
-public class ccc10s5B {
+public class ccc10s5 {
 
 	static int dp[][] = new int[2501][105];
 	static int dpnext[][] = new int[2501][105];
 	static int[] start = new int[105];
 	static ArrayList<Integer>[] adj = new ArrayList[105];
 	static String s;
-	
 	static int node_count = 1, string_pos = -1, x;
-    static void parse(int N){
-        adj[N] = new ArrayList<Integer>();
-        ++string_pos;
-        while(s.charAt(string_pos) == ' '){
-            ++string_pos;
-        }
-        //System.out.println(string_pos + " " + s.charAt(string_pos) + " " + N);
-        if (s.charAt(string_pos) == '('){
-            ++node_count;
-            adj[N].add(node_count);
-            parse(node_count);
-            ++node_count;
-            adj[N].add(node_count);
-            parse(node_count);
-            ++string_pos;
-            while(s.charAt(string_pos) == ' '){
-                ++string_pos;
-            }
-        } else {
-            int x = 0;
-            while(string_pos < s.length() && '0' <= s.charAt(string_pos) && s.charAt(string_pos) <= '9'){
-                x = x * 10 + s.charAt(string_pos) - '0';
-                ++string_pos;
-            }
-            start[N] = x;
-            --string_pos;
-        }
-    }
-	
+
+	static void parse(int N){
+		adj[N] = new ArrayList<Integer>();
+		++string_pos;
+		while(s.charAt(string_pos) == ' '){
+			++string_pos;
+		}
+		if (s.charAt(string_pos) == '('){
+			++node_count;
+			adj[N].add(node_count);
+			parse(node_count);
+			++node_count;
+			adj[N].add(node_count);
+			parse(node_count);
+			++string_pos;
+			while(s.charAt(string_pos) == ' '){
+				++string_pos;
+			}
+		} else {
+			int x = 0;
+			while(string_pos < s.length() && '0' <= s.charAt(string_pos) && s.charAt(string_pos) <= '9'){
+				x = x * 10 + s.charAt(string_pos) - '0';
+				++string_pos;
+			}
+			start[N] = x;
+			string_pos--;
+		}
+	}
+
 	public static class node {
 		node left;
 		node right;
 		node parent;
 	}
 	static node[] tree;
-
-	//	public static void build(int e, int v){
-	//		if (tree[e].left==new node()&&tree[e].right==new node()){
-	//			return build();
-	//		}
-	//	}
 
 	public static void dfs(int x, int node){
 		if (start[node]!=0){
@@ -67,30 +58,29 @@ public class ccc10s5B {
 					dp[i+l][node] = Math.max(dp[i+l][node], dpnext[i][left]+dpnext[l][right]);
 				}
 			}
-			
-			
+
+
 		}
 
-//		System.out.println(start[node]);
 		for(int i=0; i<=x; i++){
-			
 			for (int l=0; l<=x-i; l++){
-				dpnext[i+l][node] = Math.max(dpnext[i+l][node], (int)Math.min(Math.pow(l+1, 2), dp[i][node]));
-			
+				dpnext[i+l][node] = Math.max(dpnext[i+l][node], 
+							     (int)Math.min(Math.pow(l+1, 2), dp[i][node]));
 			}
 		}
-		
+
 	}
 
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 Scanner sc = new Scanner(System.in);
-	    	s = sc.nextLine();
-	        parse(1);
-	        x = sc.nextInt();
-	        dfs(x,1);
-	        System.out.println(dp[x][1]);
+		Scanner sc = new Scanner(System.in);
+		
+		s = sc.nextLine();
+		parse(1);
+		x = sc.nextInt();
+		dfs(x,1);
+		System.out.println(dp[x][1]);
 	}
 
 }
