d2cb350d10591ce6f609ab6fb71904da4fad31bd
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-02-10 10:37:48 -0500

    Create ccc11s1.java

---
+++ CCC Senior/ccc11s1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ccc11s1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+    
+		int n = sc.nextInt();
+		int countT = 0, countS = 0;
+		sc.nextLine();
+    
+		for (int i=0; i<n; i++){
+			String s = sc.nextLine();
+			String st = s.replace("t", "");
+			st = st.replace("T", "");
+			String ss = s.replace("s", "");
+			ss = ss.replace("S", "");
+			countT += s.length()-st.length();
+			countS += s.length()-ss.length();
+		}
+		
+		if (countT<=countS) {
+			System.out.println("French");
+		} else {
+			System.out.println("English");
+		}
+		
+	}
+
+}
