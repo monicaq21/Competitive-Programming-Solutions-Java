3b25c7adbaded67db55994356a2f7d05a2c5ef2c
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-02-16 20:07:05 -0500

    Create ccc15s1.java

---
+++ CCC Senior/ccc15s1.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class ccc15s1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		Stack<Integer> map = new Stack<Integer>();
+		
+		int n = sc.nextInt();
+		
+		for (int i=0; i<n; i++) {
+			int a = sc.nextInt();
+			if (a==0) map.pop();
+			else map.add(a);
+		}
+		
+		int sum = 0;
+		for (int i: map) sum+=i;
+		
+		System.out.println(sum);
+		
+	}
+}
