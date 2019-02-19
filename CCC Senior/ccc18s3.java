4500402892486b02f9d6d9162b28bd4cdfad08aa
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-02-19 17:57:48 -0500

    Update ccc18s3.java

--- CCC Senior/ccc18s3.java
+++ CCC Senior/ccc18s3.java
@@ -1,5 +1,3 @@
-package contest_questions;
-
 import java.util.LinkedList;
 import java.util.Scanner;
 
@@ -7,79 +5,233 @@ public class ccc18s3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		//not done yet
-		
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-		char[][] c = new char[n][m];
-		int[][] steps = new int[n][m];
-		for (int i=0; i<n; i++){
-			for (int l=0; l<m; l++){
-				steps[i][l] = Integer.MAX_VALUE;
+    
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		char[][] A = new char[N][M];
+		for (int r=0; r<N; r++) {
+			String S = sc.next();
+			A[r] = S.toCharArray();
+		}
+
+		int[][] step = new int[N][M];
+		for (int r=0; r<N; r++) {
+			for (int c=0; c<M; c++) {
+				if (A[r][c]=='W') {
+					step[r][c] = -1;
+				} else if (A[r][c]=='C') {
+					step[r][c] = -1;
+					for (int i=r-1; i>=0; i--) {
+						if (A[i][c]=='W') {
+							break;
+						} else if (A[i][c]=='.' || A[i][c]=='S'){
+							step[i][c] = -1;
+						}
+					}
+
+					for (int i=r+1; i<N; i++) {
+						if (A[i][c]=='W') {
+							break;
+						} else if (A[i][c]=='.'|| A[i][c]=='S'){
+							step[i][c] = -1;
+						}
+					}
+
+					for (int j=c-1; j>=0; j--) {
+						if (A[r][j]=='W') {
+							break;
+						} else if (A[r][j]=='.' || A[r][j]=='S'){
+							step[r][j] = -1;
+						}
+					}
+
+					for (int j=c+1; j<M; j++) {
+						if (A[r][j]=='W') {
+							break;
+						} else if (A[r][j]=='.' || A[r][j]=='S'){
+							step[r][j] = -1;
+						}
+					}
+
+				}
+				if (step[r][c]!=-1) {
+					step[r][c] = Integer.MAX_VALUE;
+				}
 			}
 		}
-		
-		
-		
-		int Startx = 0;
-		int Starty = 0;
-		LinkedList <Integer> directionx = new LinkedList <Integer>();
-		LinkedList <Integer> directiony = new LinkedList <Integer>();
-		LinkedList <Integer> togox = new LinkedList <Integer>();
-		LinkedList <Integer> togoy = new LinkedList <Integer>();
-		for (int i=0; i<n; i++){
-			String s = sc.nextLine();
-			c[i] = s.toCharArray();
-			if (s.contains("S")){
-				Startx = i;
-				Starty = s.indexOf("S");
-			}
-			if (s.contains(".")){
-				togox.add(i);
-				togoy.add(s.indexOf("."));
-			}
-			if (s.contains("W")){
-				steps[i][s.indexOf('W')]=-1;
-			}
-			if (s.contains("C")){
-				steps[i][s.indexOf('C')]=-1;
-			}
+		LinkedList<Integer>  x = new LinkedList<Integer>();
+		LinkedList<Integer>  y = new LinkedList<Integer>();
+		int Sr = 0;
+		int Sc = 0;
+		for (int r=0; r<N; r++) {
+			for (int c=0; c<M; c++) {
+				if (A[r][c]=='.') {
+					x.add(r);
+					y.add(c);
+				} else if (A[r][c]=='S') {
+					Sr = r;
+					Sc = c;
+				}
+			}	
 		}
-		
-		
-		
-		directionx.add(Startx);
-		directiony.add(Starty);
-		
-		int p = 0;
-		while (!directionx.isEmpty()){
-			p++;
-			int x = directionx.pop();
-			int y = directiony.pop();
-			int dotx = togox.pop();
-			int doty = togoy.pop();
-			for (int i=x-1; i<=x+1; i++){
-				for (int l=y-1; l<=y+1; l++){
-					if (c[i][l]!='C' && c[i][l]!='W'){
-						if (steps[i][l]>steps[x][y]+1){
-							steps[i][l]=steps[x][y]+1;
+
+		if (step[Sr][Sc]!=-1) {
+			step[Sr][Sc] = 0;
+		} else {
+			for (int i=0; i<x.size(); i++) {
+				System.out.println(-1);
+			}
+			return;
+		}
+
+		LinkedList<Integer> rowQ = new LinkedList<Integer>();
+		LinkedList<Integer> colQ = new LinkedList<Integer>();
+
+		rowQ.add(Sr);
+		colQ.add(Sc);
+
+		while (!rowQ.isEmpty()) {
+			int r = rowQ.poll();
+			int c = colQ.poll();
+
+			if (A[r][c]=='.'||A[r][c]=='S') {
+				if (r-1>=0 && step[r-1][c]!=-1) {
+					if (A[r-1][c]=='U' || A[r-1][c]=='D' || A[r-1][c]=='L' || A[r-1][c]=='R') {
+						if (step[r-1][c]>step[r][c]) {
+							step[r-1][c]=step[r][c];
+							rowQ.add(r-1);
+							colQ.add(c);
 						}
-						if (i==dotx && l==doty){
-							System.out.println(steps[i][l]);
+					} else {
+						if (step[r-1][c]>step[r][c]+1) {
+							step[r-1][c] = step[r][c]+1;
+							rowQ.add(r-1);
+							colQ.add(c);
 						}
-						
+					}
+
+				}
+				if (r+1<N && step[r+1][c]!=-1) {
+					if (A[r+1][c]=='U' || A[r+1][c]=='D' || A[r+1][c]=='L' || A[r+1][c]=='R') {
+						if (step[r+1][c]>step[r][c]) {
+							step[r+1][c]=step[r][c];
+							rowQ.add(r+1);
+							colQ.add(c);
+						}
+					} else {
+						if (step[r+1][c]>step[r][c]+1) {
+							step[r+1][c] = step[r][c]+1;
+							rowQ.add(r+1);
+							colQ.add(c);
+						}
+					}
+
+				}
+				if (c-1>=0 && step[r][c-1]!=-1) {
+					if (A[r][c-1]=='U' || A[r][c-1]=='D' || A[r][c-1]=='L' || A[r][c-1]=='R') {
+						if (step[r][c-1]>step[r][c]) {
+							step[r][c-1]=step[r][c];
+							rowQ.add(r);
+							colQ.add(c-1);
+						}
+					} else {
+						if (step[r][c-1]>step[r][c]+1) {
+							step[r][c-1] = step[r][c]+1;
+							rowQ.add(r);
+							colQ.add(c-1);
+						}
+					}
+				}
+				if (c+1<M && step[r][c+1]!=-1) {
+					if (A[r][c+1]=='U' || A[r][c+1]=='D' || A[r][c+1]=='L' || A[r][c+1]=='R') {
+						if (step[r][c+1]>step[r][c]) {
+							step[r][c+1]=step[r][c];
+							rowQ.add(r);
+							colQ.add(c+1);
+						}
+					} else {
+						if (step[r][c+1]>step[r][c]+1) {
+							step[r][c+1] = step[r][c]+1;
+							rowQ.add(r);
+							colQ.add(c+1);
+						}
+					}
+				}
+			} else if (A[r][c]=='U') {
+				if (r-1>=0 && step[r-1][c]!=-1) {
+					if (A[r-1][c]=='U' || A[r-1][c]=='D' || A[r-1][c]=='L' || A[r-1][c]=='R') {
+						if (step[r-1][c]>step[r][c]) {
+							step[r-1][c]=step[r][c];
+							rowQ.add(r-1);
+							colQ.add(c);
+						}
+					} else {
+						if (step[r-1][c]>step[r][c]+1) {
+							step[r-1][c] = step[r][c]+1;
+							rowQ.add(r-1);
+							colQ.add(c);
+						}
+					}
+				}
+			} else if (A[r][c]=='D') {
+				if (r+1<N && step[r+1][c]!=-1) {
+					if (A[r+1][c]=='U' || A[r+1][c]=='D' || A[r+1][c]=='L' || A[r+1][c]=='R') {
+						if (step[r+1][c]>step[r][c]) {
+							step[r+1][c]=step[r][c];
+							rowQ.add(r+1);
+							colQ.add(c);
+						}
+					} else {
+						if (step[r+1][c]>step[r][c]+1) {
+							step[r+1][c] = step[r][c]+1;
+							rowQ.add(r+1);
+							colQ.add(c);
+						}
+					}
+				}
+			} else if (A[r][c]=='L') {
+				if (c-1>=0 && step[r][c-1]!=-1) {
+					if (A[r][c-1]=='U' || A[r][c-1]=='D' || A[r][c-1]=='L' || A[r][c-1]=='R') {
+						if (step[r][c-1]>step[r][c]) {
+							step[r][c-1]=step[r][c];
+							rowQ.add(r);
+							colQ.add(c-1);
+						}
+					} else {
+						if (step[r][c-1]>step[r][c]+1) {
+							step[r][c-1] = step[r][c]+1;
+							rowQ.add(r);
+							colQ.add(c-1);
+						}
+					}
+				}
+			} else if (A[r][c]=='R') {
+				if (c+1<M && step[r][c+1]!=-1) {
+					if (A[r][c+1]=='U' || A[r][c+1]=='D' || A[r][c+1]=='L' || A[r][c+1]=='R') {
+						if (step[r][c+1]>step[r][c]) {
+							step[r][c+1]=step[r][c];
+							rowQ.add(r);
+							colQ.add(c+1);
+						}
+					} else {
+						if (step[r][c+1]>step[r][c]+1) {
+							step[r][c+1] = step[r][c]+1;
+							rowQ.add(r);
+							colQ.add(c+1);
+						}
 					}
 				}
 			}
-			
 		}
-		
-		
-		
-		
-		
-	}
 
+		for (int i=0; i<x.size(); i++) {
+			int s = step[x.get(i)][y.get(i)];
+			if (s==-1||s==Integer.MAX_VALUE) {
+				System.out.println(-1);
+			} else {
+				System.out.println(s);
+			}
+		}
+	}
 }
