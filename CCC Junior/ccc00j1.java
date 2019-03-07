12b910b33a5c9f4eac3d4a1f0602b099b6624e31
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-03-07 16:52:37 -0500

    Update ccc00j1.java

--- CCC Junior/ccc00j1.java
+++ CCC Junior/ccc00j1.java
@@ -11,32 +11,34 @@ public class ccc00j1 {
 
 		System.out.println("Sun Mon Tue Wed Thr Fri Sat");
 
-		for (int i=0; i<start-1; i++){
+		for (int i = 0; i < start - 1; i++) {
 			System.out.print("    ");
 		}
-		for (int i=1; i<days+1; i++){
-			if (i<10){
-				if ((start+i-1)%7==0){
-					System.out.print("  "+i);
-				} else{
-					System.out.print("  "+i+" ");
+		
+		for (int i = 1; i < days + 1; i++){
+			if (i<10) {
+				if ((start + i - 1) % 7 == 0) {
+					System.out.print("  " + i);
+				} else {
+					System.out.print("  " + i + " ");
 				}
 
 			} else {
-				if ((i+start-1)%7==0){
-					System.out.print(" "+i);
-				} else if (i==days){
-					System.out.print(" "+i);
-				} else{
-					System.out.print(" "+i+" ");
+				if ((i + start - 1) % 7 == 0) {
+					System.out.print(" " + i);
+				} else if (i == days) {
+					System.out.print(" " + i);
+				} else {
+					System.out.print(" " + i + " ");
 				}
 			}
-			if ((i+start-1)%7==0){
+			
+			if ((i + start - 1) % 7 == 0) {
 				System.out.println();
 			}
-
 		}
-		if (days%7!=0){
+
+		if (days % 7 != 0) {
 			System.out.println();
 		}
 
