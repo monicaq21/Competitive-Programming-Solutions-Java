9b283fab936edf9aefde2885a21ff5ae22b307e3
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-03-13 15:45:36 -0400

    Update ccc03j1.java

--- CCC Junior/ccc03j1.java
+++ CCC Junior/ccc03j1.java
@@ -8,88 +8,87 @@ public class ccc03j1 {
 		
 		int t = sc.nextInt();
 		int s = sc.nextInt();
-		sc.nextLine();
 		int h = sc.nextInt();
 		
-		if (s==2){
-			for (int i=0; i<t; i++) {
+		if (s == 2){
+			for (int i = 0; i < t; i++) {
 				System.out.println("*  *  *");
 			}
 			System.out.println("*******");
-			for (int i=0; i<h; i++){
+			for (int i = 0; i < h; i++){
 				System.out.println("   *");
 			}
 
-		} else if (s==3){
-			for (int i=0; i<t; i++) {
+		} else if (s == 3){
+			for (int i = 0; i < t; i++) {
 				System.out.println("*   *   *");
 		}
 			System.out.println("*********");
-			for (int i=0; i<h; i++){
+			for (int i = 0; i < h; i++){
 				System.out.println("    *");
 			}
-		} else if (s==4){
-			for (int i=0; i<t; i++) {
+		} else if (s == 4){
+			for (int i = 0; i < t; i++) {
 				System.out.println("*    *    *");
 		}
 			System.out.println("***********");
-			for (int i=0; i<h; i++){
+			for (int i = 0; i < h; i++){
 				System.out.println("     *");
 			}
-		} else if (s==5){
-			for (int i=0; i<t; i++) {
+		} else if (s == 5){
+			for (int i = 0; i < t; i++) {
 				System.out.println("*     *     *");
 		}
 			System.out.println("*************");
-			for (int i=0; i<h; i++){
+			for (int i = 0; i < h; i++){
 				System.out.println("      *");
 			}
-		} else if (s==6){
-			for (int i=0; i<t; i++) {
+		} else if (s == 6){
+			for (int i = 0; i < t; i++) {
 				System.out.println("*      *      *");
 		}
 			System.out.println("***************");
-			for (int i=0; i<h; i++){
+			for (int i = 0; i < h; i++){
 				System.out.println("       *");
 			}
-		} else if (s==7){
-			for (int i=0; i<t; i++) {
+		} else if (s == 7){
+			for (int i = 0; i < t; i++) {
 				System.out.println("*       *       *");
 		}
 			System.out.println("*****************");
-			for (int i=0; i<h; i++){
+			for (int i = 0; i < h; i++){
 				System.out.println("        *");
 			}
-		} else if (s==8){
-			for (int i=0; i<t; i++) {
+		} else if (s == 8){
+			for (int i = 0; i < t; i++) {
 				System.out.println("*        *        *");
 		}
 			System.out.println("*******************");
-			for (int i=0; i<h; i++){
+			for (int i = 0; i < h; i++){
 				System.out.println("         *");
 			}
-		} else if (s==9){
-			for (int i=0; i<t; i++) {
+		} else if (s == 9){
+			for (int i = 0; i < t; i++) {
 				System.out.println("*         *         *");
 		}
 			System.out.println("*********************");
-			for (int i=0; i<h; i++){
+			for (int i = 0; i < h; i++){
 				System.out.println("          *");
 			}
-		} else if (s==10){
-			for (int i=0; i<t; i++) {
+		} else if (s == 10){
+			for (int i = 0; i < t; i++) {
 				System.out.println("*          *          *");
-		}			System.out.println("***********************");
-		for (int i=0; i<h; i++){
-			System.out.println("           *");
-		}
-		
+			}			
+			System.out.println("***********************");
+			for (int i = 0; i < h; i++){
+				System.out.println("           *");
+			}
 		} else {
-			for (int i=0; i<t; i++){
+			for (int i = 0; i < t; i++){
 				System.out.println("***");
 		}
 			System.out.println("***");
-			for (int i=0; i<h; i++){
+			for (int i = 0; i < h; i++){
 				System.out.println(" *");
 			}
 		}
