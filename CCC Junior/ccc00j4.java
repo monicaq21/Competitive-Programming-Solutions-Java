110899d869953eeb699c4498e199d9601ff8cd11
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-03-13 00:04:56 -0400

    Update ccc00j4.java

--- CCC Junior/ccc00j4.java
+++ CCC Junior/ccc00j4.java
@@ -9,28 +9,29 @@ public class ccc00j4 {
 		LinkedList <Integer> l = new LinkedList <Integer>();
 		
 		int n = sc.nextInt();
-		for (int i=0; i<n; i++){
+		
+		for (int i = 0; i < n; i++) {
 			int k = sc.nextInt();
 			l.add(k);
 		}
 		
-		while (true){
+		while (true) {
 			int a = sc.nextInt();
-			if (a==77){
-				for (int i=0; i<l.size(); i++) {
-					System.out.print(l.get(i)+" ");
+			if (a == 77){
+				for (int i = 0; i < l.size(); i++) {
+					System.out.print(l.get(i) + " ");
 				}
 				break;
-			} else if (a==99) {
+			} else if (a == 99) {
 				int b = sc.nextInt()-1;
 				int c = sc.nextInt();
-				double left = (c/100.0)*l.get(b);
-				l.add(b+1, l.get(b)-(int)left);
+				double left = (c / 100.0) * l.get(b);
+				l.add(b + 1, l.get(b) - (int)left);
 				l.set(b, (int)left);
 			} else {
-				int b = sc.nextInt()-1;
-				l.set(b, l.get(b)+l.get(b+1));
-				l.remove(b+1);
+				int b = sc.nextInt() - 1;
+				l.set(b, l.get(b) + l.get(b + 1));
+				l.remove(b + 1);
 			}
 		}
 		
