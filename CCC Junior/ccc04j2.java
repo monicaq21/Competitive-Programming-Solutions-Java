8b2d165d71751f01126b60174b4f0eef44957b39
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-03-14 23:37:43 -0400

    Update ccc04j2.java

--- CCC Junior/ccc04j2.java
+++ CCC Junior/ccc04j2.java
@@ -10,14 +10,9 @@ public class ccc04j2 {
 		int a = sc.nextInt();
 		int b = sc.nextInt();
 		
-		
-		for (int i=a; i<=b; i=i+60){
-			System.out.println("All positions change in year "+i);
+		for (int i = a; i <= b; i = i + 60){
+			System.out.println("All positions change in year " + i);
 		}
-		
-		
-		
-		
 		
 	}
 
