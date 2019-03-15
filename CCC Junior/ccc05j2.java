9048249cfe2dedb63d0b3512b949e47826784034
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-03-15 01:46:08 -0400

    Update ccc05j2.java

--- CCC Junior/ccc05j2.java
+++ CCC Junior/ccc05j2.java
@@ -8,18 +8,18 @@ public class ccc05j2 {
 		Scanner sc = new Scanner(System.in);
 		
 		int A = sc.nextInt(), 
-				B = sc.nextInt(), 
-				count = 0,
-				Count = 0;
+			B = sc.nextInt(), 
+			count = 0,
+			Count = 0;
 
-		for (int k=A; k<=B; k++){
-			for (int i=1; i<=k; i++){
-				if (k%i==0) count++;
+		for (int k = A; k <= B; k++){
+			for (int i = 1; i <= k; i++){
+				if (k % i == 0) count++;
 			} 
-			if (count==4) Count++;
+			if (count == 4) Count++;
 			count = 0;
 		}
-		System.out.println("The number of RSA numbers between "+A+" and "+B+" is "+Count);
+		System.out.println("The number of RSA numbers between " + A + " and " + B + " is " + Count);
 	}
 
 }
