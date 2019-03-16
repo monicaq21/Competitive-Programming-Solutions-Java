946b02ab59407d719793e2ca7696128f1650aa2a
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-03-16 16:30:59 -0400

    Update ccc09j2.java

--- CCC Junior/ccc09j2.java
+++ CCC Junior/ccc09j2.java
@@ -5,24 +5,25 @@ public class ccc09j2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
+		
 		int trout = sc.nextInt(),
 				pike = sc.nextInt(),
 				pickerel = sc.nextInt(),
 				ways = sc.nextInt(),
-				maxtrout = ways/trout,
-				maxpike = ways/pike,
-				maxpickerel = ways/pickerel,
+				maxtrout = ways / trout,
+				maxpike = ways / pike,
+				maxpickerel = ways / pickerel,
 				count = 0;
 
-		for (int j=0; j<=maxpickerel; j++)
-			for (int l=0; l<=maxpike; l++)
-				for (int i=0; i<=maxtrout; i++)
-					if (i*trout+j*pickerel+l*pike<=ways && i*trout+j*pickerel+l*pike!=0){
-						System.out.println(i+" Brown Trout, "+l+" Northern Pike, "+j+" Yellow Pickerel");
+		for (int j = 0; j <= maxpickerel; j++)
+			for (int l = 0; l <= maxpike; l++)
+				for (int i = 0; i <= maxtrout; i++)
+					if ((i * trout + j * pickerel + l * pike <= ways) && (i * trout + j * pickerel + l * pike != 0)){
+						System.out.println(i + " Brown Trout, " + l + " Northern Pike, " + j + " Yellow Pickerel");
 						count++;
 					}
 
-		System.out.println("Number of ways to catch fish: "+count);
+		System.out.println("Number of ways to catch fish: " + count);
 
 
 	}
