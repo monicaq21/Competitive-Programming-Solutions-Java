35a4eb6507ad9748e331f8fbe16d8c07ff5b3c64
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-03-16 00:35:32 -0400

    Update ccc06j1.java

--- CCC Junior/ccc06j1.java
+++ CCC Junior/ccc06j1.java
@@ -12,27 +12,27 @@ public class ccc06j1 {
 				drink = sc.nextInt(),
 				dessert = sc.nextInt();
 		
-		if (burger==1) burger = 461;
-		else if (burger==2) burger = 431;
-		else if (burger==3) burger = 420;
+		if (burger == 1) burger = 461;
+		else if (burger == 2) burger = 431;
+		else if (burger == 3) burger = 420;
 		else burger = 0;
 		
-		if (side==1) side = 100;
-		else if (side==2) side = 57;
-		else if (side==3) side = 70;
+		if (side == 1) side = 100;
+		else if (side == 2) side = 57;
+		else if (side == 3) side = 70;
 		else side = 0;
 		
-		if (drink==1) drink = 130;
-		else if (drink==2) drink = 160;
-		else if (drink==3) drink = 118;
+		if (drink == 1) drink = 130;
+		else if (drink == 2) drink = 160;
+		else if (drink == 3) drink = 118;
 		else drink = 0;
 
-		if (dessert==1) dessert = 167;
-		else if (dessert==2) dessert = 266;
-		else if (dessert==3) dessert = 75;
+		if (dessert == 1) dessert = 167;
+		else if (dessert == 2) dessert = 266;
+		else if (dessert == 3) dessert = 75;
 		else dessert = 0;
 
-		System.out.println("Your total Calorie count is " + (burger+side+drink+dessert) + ".");
+		System.out.println("Your total Calorie count is " + (burger + side + drink + dessert) + ".");
 		
 	}
 
