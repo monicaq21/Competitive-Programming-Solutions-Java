aa313f628dde1db291e5bccee4333f2b8d0eade9
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-03-16 00:37:52 -0400

    Update ccc06j3.java

--- CCC Junior/ccc06j3.java
+++ CCC Junior/ccc06j3.java
@@ -8,23 +8,25 @@ public class ccc06j3 {
 		
 		int[] a = {1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,4,1,2,3,1,2,3,4};
 		int[] b = {2,2,2,3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,7,8,8,8,9,9,9,9};
-		while (true){
+		
+		while (true) {
 			int count = 0;
 			String s = sc.nextLine();
 			if (s.equals("halt")) break;
 			
-			int A = a[s.charAt(0)-97];
-			count=A;
-			if (s.length()==1){
+			int A = a[s.charAt(0) - 97];
+			count = A;
+			
+			if (s.length() == 1){
 				System.out.println(A);
 				continue;
 			}
 			
-			for (int i=1; i<s.length(); i++){
-				if (b[s.charAt(i)-97]!=b[s.charAt(i-1)-97]){
-					count+=a[s.charAt(i)-97];
+			for (int i = 1; i < s.length(); i++){
+				if (b[s.charAt(i) - 97] != b[s.charAt(i - 1) - 97]) {
+					count += a[s.charAt(i) - 97];
 				} else {
-					count+=a[s.charAt(i)-97]+2;
+					count += a[s.charAt(i) - 97] + 2;
 				}
 			}
 			System.out.println(count);
