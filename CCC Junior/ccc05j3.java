f083fca912aaef8014dfc3424b8de67b970bf4de
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-03-16 00:33:58 -0400

    Update ccc05j3.java

--- CCC Junior/ccc05j3.java
+++ CCC Junior/ccc05j3.java
@@ -6,30 +6,32 @@ public class ccc05j3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
+		
 		Stack <String> s = new Stack <String>();
 		
-		while (true){
+		while (true) {
 			String S = sc.next();
-			if (S.equals("SCHOOL")){
-				break;
-			}
+			if (S.equals("SCHOOL")) break;
 			s.push(S);
 		}
 		
-		while (true){
+		while (true) {
 			String direction = s.pop();
 			String D = "";
-			if (direction.equals("L")){
+			
+			if (direction.equals("L")) {
 				D = "RIGHT";
 			} else {
 				D = "LEFT";
 			}
-			if (s.isEmpty()){
-				System.out.println("Turn "+D+" into your HOME.");
+			
+			if (s.isEmpty()) {
+				System.out.println("Turn " + D + " into your HOME.");
 				break;
 			}
+			
 			String street = s.pop();
-			System.out.println("Turn "+D+" onto "+street+" street.");
+			System.out.println("Turn " + D + " onto " + street + " street.");
 			
 		}
 		
