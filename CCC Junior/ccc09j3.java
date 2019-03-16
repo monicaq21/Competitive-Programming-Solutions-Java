ea7cd70e21ad9f6a3541170debc892f5d83e517a
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-03-16 16:33:09 -0400

    Update ccc09j3.java

--- CCC Junior/ccc09j3.java
+++ CCC Junior/ccc09j3.java
@@ -9,42 +9,42 @@ public class ccc09j3 {
 		String S = sc.nextLine();
 		
 		int a = Integer.parseInt(S);
-		String k = Integer.toString(a,60);
+		String k = Integer.toString(a, 60);
 		a = Integer.parseInt(k);
 		
-		int victoria = a-300;
-		if (victoria<0){
-			victoria = victoria+2400;
+		int victoria = a - 300;
+		if (victoria < 0){
+			victoria = victoria + 2400;
 		}
-		int edmonton = a-200;
-		if (edmonton<0){
-			edmonton = edmonton+2400;
+		int edmonton = a - 200;
+		if (edmonton < 0){
+			edmonton = edmonton + 2400;
 		}
-		int winnipeg = a-100;
-		if (winnipeg<0){
-			winnipeg = winnipeg+2400;
+		int winnipeg = a - 100;
+		if (winnipeg < 0){
+			winnipeg = winnipeg + 2400;
 		}
-		int halifax = a+100;
-		if (halifax>=2400){
-			halifax = halifax-2400;
+		int halifax = a + 100;
+		if (halifax >= 2400){
+			halifax = halifax - 2400;
 		}
-		int john = a+130;
-		if (john>=2400){
-			john = john-2400;
+		int john = a + 130;
+		if (john >= 2400){
+			john = john - 2400;
 		}
-		if (john%100>=60){
-			john-=60;
-			john+=100;
+		if (john % 100 >= 60){
+			john -= 60;
+			john += 100;
 		}
 		
 
-		System.out.println(a+" in Ottawa");
-		System.out.println(victoria+" in Victoria");
-		System.out.println(edmonton+" in Edmonton");
-		System.out.println(winnipeg+" in Winnipeg");
-		System.out.println(a+" in Toronto");
-		System.out.println(halifax+" in Halifax");
-		System.out.print(john+" in St. John's");
+		System.out.println(a + " in Ottawa");
+		System.out.println(victoria + " in Victoria");
+		System.out.println(edmonton + " in Edmonton");
+		System.out.println(winnipeg + " in Winnipeg");
+		System.out.println(a + " in Toronto");
+		System.out.println(halifax + " in Halifax");
+		System.out.print(john + " in St. John's");
 
 	}
 
