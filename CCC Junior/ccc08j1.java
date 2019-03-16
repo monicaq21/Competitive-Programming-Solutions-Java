950c11e45fed2468208ccc45ef49db4a3b3d5d36
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-03-16 00:46:00 -0400

    Update ccc08j1.java

--- CCC Junior/ccc08j1.java
+++ CCC Junior/ccc08j1.java
@@ -4,12 +4,12 @@ public class ccc08j1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		Scanner sc = new Scanner(System.in);
+		
 		double weight = sc.nextDouble(), height = sc.nextDouble();
     
-		if (weight/(height*height) > 25) System.out.println("Overweight");
-		else if (weight/(height*height) < 18.5) System.out.println("Underweight");
+		if (weight / (height * height) > 25) System.out.println("Overweight");
+		else if (weight / (height * height) < 18.5) System.out.println("Underweight");
 		else System.out.println("Normal weight");
     
 	}
