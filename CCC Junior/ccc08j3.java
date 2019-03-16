2f97f99c758181c41c466c2149cae42198980a33
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-03-16 16:23:58 -0400

    Update ccc08j3.java

--- CCC Junior/ccc08j3.java
+++ CCC Junior/ccc08j3.java
@@ -6,22 +6,21 @@ public class ccc08j3 {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 
-		//GPS Text Entry
 		String s = sc.nextLine();
-		char[][] A = {{'A','B','C','D','E','F'},
-				{'G','H','I','J','K','L'},
-				{'M','N','O','P','Q','R'},
-				{'S','T','U','V','W','X'},
-				{'Y','Z',' ','-','.','*'}
-		};
+		char[][] A = {{'A', 'B', 'C', 'D', 'E', 'F'},
+				{'G', 'H', 'I', 'J', 'K', 'L'},
+				{'M', 'N', 'O', 'P', 'Q', 'R'},
+				{'S', 'T', 'U', 'V', 'W', 'X'},
+				{'Y', 'Z', ' ', '-', '.', '*'}};
+		
 		int beginrow = 0, begincol = 0, step = 0;
     
-		for (int i=0; i<s.length(); i++){
+		for (int i = 0; i < s.length(); i++){
 			char c = s.charAt(i);
-			for (int row = 0; row<5; row++){
-				for (int col = 0; col<6; col++){
+			for (int row = 0; row < 5; row++){
+				for (int col = 0; col < 6; col++){
 					if (A[row][col] == s.charAt(i)){
-						step = step+Math.abs(row-beginrow)+Math.abs(col-begincol);
+						step = step + Math.abs(row - beginrow) + Math.abs(col - begincol);
 						beginrow = row;
 						begincol = col;
 					}
@@ -31,7 +30,7 @@ public class ccc08j3 {
 
 
 		}
-		step = step+Math.abs(4-beginrow)+Math.abs(5-begincol);
+		step = step + Math.abs(4 - beginrow) + Math.abs(5 - begincol);
 		System.out.println(step);
 
 	}
