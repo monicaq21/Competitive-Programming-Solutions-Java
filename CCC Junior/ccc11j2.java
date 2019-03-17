7e7361683d14e4f78d5c135a070b952f96ff8e13
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-03-17 00:26:22 -0400

    Update ccc11j2.java

--- CCC Junior/ccc11j2.java
+++ CCC Junior/ccc11j2.java
@@ -11,12 +11,12 @@ public class ccc11j2 {
 		int m = sc.nextInt();
 		boolean flag = true;
 		
-		for (int i=1; i<=m; i++){
-			double a =-6*Math.pow(i, 4)+Math.pow(i,3)*h+Math.pow(i, 2)*2+i;
-			if (a<=0){
+		for (int i = 1; i <= m; i++){
+			double a = -6 * Math.pow(i, 4) + Math.pow(i, 3) * h + Math.pow(i, 2) * 2 + i;
+			if (a <= 0){
 				System.out.println("The balloon first touches ground at hour: ");
 				System.out.println(i);
-				flag=false;
+				flag = false;
 				break;
 			}
 		}
