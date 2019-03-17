f13dfb99a4e7d4bc02016559b4e79168ebec3349
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-03-17 00:38:19 -0400

    Update ccc13j1.java

--- CCC Junior/ccc13j1.java
+++ CCC Junior/ccc13j1.java
@@ -4,12 +4,11 @@ public class ccc13j1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
 		Scanner sc = new Scanner(System.in);
 		
 		int y = sc.nextInt();
 		int m = sc.nextInt();
-		System.out.println(m+(m-y));
+		System.out.println(m + (m - y));
 		
 		
 		
