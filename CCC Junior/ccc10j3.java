bffb86faad1ecfa2bd12a9e151983574ae18183e
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-03-17 00:16:12 -0400

    Update ccc10j3.java

--- CCC Junior/ccc10j3.java
+++ CCC Junior/ccc10j3.java
@@ -6,95 +6,91 @@ public class ccc10j3 {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 
-		int a =0, b =0;
+		int a = 0, b = 0;
     
-		while (true){
+		while (true) {
 			String s = sc.nextLine();
-			if (s.startsWith("7")){
-				return;
-			} else if (s.startsWith("1")){
+			if (s.startsWith("7")) return;
+			
+			if (s.startsWith("1")){
 				String[] t = s.split(" ");
 				if (t[1].equals("A")){
 					if (!s.contains("-")){
-						int ww = Integer.parseInt(t[2]);
-						a=ww;
+						a = Integer.parseInt(t[2]);
 					} else {
-						t[2]=t[2].replace("-", "");
-						int ww = Integer.parseInt(t[2]);
-						a=ww*(-1);
+						t[2] = t[2].replace("-", "");
+						a = (-1) * Integer.parseInt(t[2]);
 					}
 				} else {
 					if (!s.contains("-")){
-						int ww = Integer.parseInt(t[2]);
-						b=ww;
+						b = Integer.parseInt(t[2]);
 					} else {
-						t[2]=t[2].replace("-", "");
-						int ww = Integer.parseInt(t[2]);
-						b=ww*(-1);
+						t[2] = t[2].replace("-", "");
+						b = (-1) * Integer.parseInt(t[2]);
 					}
 				}
 
 			} else if (s.startsWith("2")){
-				if (s.charAt(2)=='A'){
+				if (s.charAt(2) == 'A'){
 					System.out.println(a);
 				} else {
 					System.out.println(b);
 				}
 			} else if (s.startsWith("3")){
-				if (s.charAt(2)=='A'){
-					if (s.charAt(4)=='B'){
-						a+=b;
-					} else{
-						a*=2;
+				if (s.charAt(2) == 'A'){
+					if (s.charAt(4) == 'B'){
+						a += b;
+					} else {
+						a *= 2;
 					}
 				} else {
-					if (s.charAt(4)=='A'){
-						b+=a;
-					} else{
-						b*=2;
+					if (s.charAt(4) == 'A'){
+						b += a;
+					} else {
+						b *= 2;
 					}
 				}
 			} else if (s.startsWith("4")){
-				if (s.charAt(2)=='A'){
-					if (s.charAt(4)=='B'){
-						a*=b;
+				if (s.charAt(2) == 'A'){
+					if (s.charAt(4) == 'B'){
+						a *= b;
 					} else{
-						a*=a;
+						a *= a;
 					}
 				} else {
-					if (s.charAt(4)=='B'){
-						b*=b;
-					} else{
-						b*=a;
+					if (s.charAt(4) == 'B'){
+						b *= b;
+					} else {
+						b *= a;
 					}
 				}
 			} else if (s.startsWith("5")){
-				if (s.charAt(2)=='A'){
-					if (s.charAt(4)=='B'){
-						a-=b;
-					} else{
-						a=0;
+				if (s.charAt(2) == 'A'){
+					if (s.charAt(4) == 'B'){
+						a -= b;
+					} else {
+						a = 0;
 					}
 				} else {
-					if (s.charAt(4)=='B'){
-						b=0;
-					} else{
-						b-=a;
+					if (s.charAt(4) == 'B'){
+						b = 0;
+					} else {
+						b -= a;
 					}
 				}
-			} else{
-				if (s.charAt(2)=='A'){
-					if (s.charAt(4)=='B'){
-						a=(int)(a/b);
-					} else{
-						a=1;
+			} else {
+				if (s.charAt(2) == 'A'){
+					if (s.charAt(4) == 'B'){
+						a = (int)(a / b);
+					} else {
+						a = 1;
 					}
 
 				} else {
-					if (s.charAt(4)=='B'){
-						b=1;
-					} else{
-						b=(int)(b/a);
+					if (s.charAt(4) == 'B'){
+						b = 1;
+					} else {
+						b = (int)(b / a);
 					}
 				}
 
