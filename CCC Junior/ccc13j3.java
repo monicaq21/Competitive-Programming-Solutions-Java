152e267b81a7e78c5923688d96b154fbab750a46
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-03-17 00:40:20 -0400

    Update ccc13j3.java

--- CCC Junior/ccc13j3.java
+++ CCC Junior/ccc13j3.java
@@ -5,15 +5,16 @@ public class ccc13j3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
+		
 		int year = sc.nextInt();
 		int begin = year;
-    
+    		
 		while (true) {
 			begin++;
 			if (isD(begin)){
 				System.out.println(begin);
-						break;
-					}
+				break;
+			}
 		}
 	}
 
@@ -21,11 +22,12 @@ public class ccc13j3 {
 	public static boolean isD(int year) {
 		// TODO Auto-generated method stub
     
-		String s = ""+year;
+		String s = "" + year;
 		char[] a = s.toCharArray();
 		Arrays.sort(a);
-		for (int i=0; i<a.length; i++){
-			if (i!=a.length-1&&a[i]==a[i+1]){
+		
+		for (int i = 0; i < a.length; i++){
+			if (i != a.length - 1 && a[i] == a[i + 1]){
 				return false;
 			}
 		}
