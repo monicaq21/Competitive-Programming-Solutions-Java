5cffb77509d90dda54cbe84ddab20f511afdd9b2
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-03-17 00:41:09 -0400

    Update ccc13j4.java

--- CCC Junior/ccc13j4.java
+++ CCC Junior/ccc13j4.java
@@ -7,21 +7,22 @@ public class ccc13j4 {
 	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
+		
 		int t = sc.nextInt();
 		int c = sc.nextInt();
 		int[] a = new int[c];
 		int count = 0;
 		int countt = 0;
 
-		for (int i=0; i<c; i++){
-			a[i]=sc.nextInt();
+		for (int i = 0; i < c; i++){
+			a[i] = sc.nextInt();
 		}
 		Arrays.sort(a);
 
-		for (int i=0; i<c; i++){
+		for (int i = 0; i < c; i++){
 			count++;
-			countt+=a[i];
-			if (countt>t){
+			countt += a[i];
+			if (countt > t){
 				count--;
 				break;
 			}
