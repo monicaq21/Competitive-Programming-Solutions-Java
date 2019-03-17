e0908c7a71e922cc7e7d98c7ea390decf2f9575a
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-03-17 00:35:03 -0400

    Update ccc12j2.java

--- CCC Junior/ccc12j2.java
+++ CCC Junior/ccc12j2.java
@@ -3,19 +3,20 @@ public class ccc12j2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		Scanner sc = new Scanner(System.in);
+		
 		int depth1 = sc.nextInt();
 		int depth2 = sc.nextInt();
 		int depth3 = sc.nextInt();
 		int depth4 = sc.nextInt();
-		if (depth1<depth2 && depth2<depth3 && depth3<depth4){
+		
+		if (depth1 < depth2 && depth2 < depth3 && depth3 < depth4){
 			System.out.println("Fish Rising");
-		} else if (depth1>depth2 && depth2>depth3 && depth3>depth4){
+		} else if (depth1 > depth2 && depth2 > depth3 && depth3 > depth4){
 			System.out.println("Fish Diving");
-		} else if (depth1==depth2 && depth2==depth3 && depth3==depth4){
+		} else if (depth1 == depth2 && depth2 == depth3 && depth3 == depth4){
 			System.out.println("Fish At Constant Depth");
-		} else{
+		} else {
 			System.out.println("No Fish");
 		}
 	}
