b0677bb895103408f133b9b99e6d4b6a781a489c
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-03-17 00:17:57 -0400

    Update ccc10j4.java

--- CCC Junior/ccc10j4.java
+++ CCC Junior/ccc10j4.java
@@ -8,29 +8,27 @@ public class ccc10j4 {
 		
 		while (true) {
 			int n = sc.nextInt();
-			if (n==0) {
-				break;
-			}
-			if(n == 1){
+			if (n == 0) break;
+			if (n == 1){
 				n = sc.nextInt();
 				System.out.println(0);
 				continue;
 			}
 			
 			int[] orig = new int[n];
-			for (int i=0; i<n; i++) {
+			for (int i = 0; i < n; i++) {
 				orig[i] = sc.nextInt();
 			}
-			int[] diff = new int[n-1];
-			for (int i=0; i<n-1; i++) {
-				diff[i] = orig[i+1] - orig[i];
+			
+			int[] diff = new int[n - 1];
+			for (int i = 0; i < n - 1; i++) {
+				diff[i] = orig[i + 1] - orig[i];
 			}
 
-			for (int l=1; l<=n; l++) { 
-				//l is the length difference
+			for (int l = 1; l <= n; l++) { 
 				boolean flag = true;
-				for (int i=0; i<n-1-l; i++) {
-					if (diff[i]!=diff[i+l]) {
+				for (int i = 0; i < n - 1 - l; i++) {
+					if (diff[i] != diff[i + l]) {
 						flag = false;
 						break;
 					}
