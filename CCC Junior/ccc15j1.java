2a64938cd09e2b8a72d12c767135f9bb3af8b3a6
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-03-18 00:37:04 -0400

    Update ccc15j1.java

--- CCC Junior/ccc15j1.java
+++ CCC Junior/ccc15j1.java
@@ -4,15 +4,16 @@ public class ccc15j1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		Scanner sc = new Scanner(System.in);
+		
 		int month = sc.nextInt();
 		int day = sc.nextInt();
-		if (month==1 || month==2 && day<18){
+		
+		if (month == 1 || month == 2 && day < 18){
 			System.out.println("Before");
-		} else if (month>2 || month==2 && day>18){
+		} else if (month > 2 || month == 2 && day > 18){
 			System.out.println("After");
-		} else if (month==2 && day==18){
+		} else if (month == 2 && day == 18){
 			System.out.println("Special");
 		}
 		
