f18988ef6f9f9f0afa4d9057d41070889da419eb
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-03-18 00:29:57 -0400

    Update ccc14j1.java

--- CCC Junior/ccc14j1.java
+++ CCC Junior/ccc14j1.java
@@ -4,12 +4,13 @@ public class ccc14j1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
 		Scanner sc = new Scanner(System.in);
+		
 		int angle1 = sc.nextInt();
 		int angle2 = sc.nextInt();
 		int angle3 = sc.nextInt();
-		if (angle1+angle2+angle3 != 180){
+		
+		if (angle1 + angle2 + angle3 != 180){
 			System.out.println("Error");
 		} else if (angle1 == angle2 && angle2 == angle3){
 				System.out.println("Equilateral");
