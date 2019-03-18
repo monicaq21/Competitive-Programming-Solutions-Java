f409d074adc6e4c9825496845a2004cf0640673a
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-03-18 00:34:56 -0400

    Update ccc14j4.java

--- CCC Junior/ccc14j4.java
+++ CCC Junior/ccc14j4.java
@@ -10,19 +10,22 @@ public class ccc14j4 {
 		int k = sc.nextInt();
 		int m = sc.nextInt();
 		LinkedList <Integer> L = new LinkedList <Integer>();
-		for (int i=1; i<=k; i++){
+		
+		for (int i = 1; i <= k; i++){
 			L.add(i);
 		}
-		for (int i=0; i<m; i++){
+		
+		for (int i = 0; i < m; i++){
 			int a = sc.nextInt();
-			for (int l=L.size()-1; l>=0; l--){
-				if ((l+1)%a==0)
+			
+			for (int l = L.size() - 1; l >= 0; l--){
+				if ((l + 1) % a == 0)
 					L.remove(l);
 			}
 		} 	
 
 
-		for (int i=0; i<L.size(); i++){
+		for (int i = 0; i < L.size(); i++){
 			System.out.println(L.get(i));
 		}
 
