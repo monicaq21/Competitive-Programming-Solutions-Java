0e3b36abea35bcaeca7f68be6bf2d149fb2e1a85
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-03-19 01:06:12 -0400

    Update ccc18j4.java

--- CCC Junior/ccc18j4.java
+++ CCC Junior/ccc18j4.java
@@ -10,10 +10,12 @@ public class ccc18j4 {
 		int n = sc.nextInt();
 		int[][] a = new int[n][n];
 		int max = 0, posx = 0, posy = 0;
-		for (int i=0; i<n; i++) {
-			for (int l=0; l<n; l++) {
+		
+		for (int i = 0; i < n; i++) {
+			for (int l = 0; l < n; l++) {
 				a[i][l] = sc.nextInt();
-				if (a[i][l]>max) {
+				
+				if (a[i][l] > max) {
 					max = a[i][l];
 					posx = i;
 					posy = l;
@@ -21,34 +23,35 @@ public class ccc18j4 {
 			}
 		}
     
-		if (posx==n-1&&posy==n-1) {
-			for (int i=0; i<n; i++) {
-				for (int l=0; l<n; l++) {
-					System.out.print(a[i][l]+" ");
-				}
-				System.out.println();
-			}
-		} else if (posx==n-1) {
-			for (int l=n-1; l>=0; l--) {
-				for (int i=0; i<n; i++) {
-					System.out.print(a[i][l]+" ");
+		if (posx == n - 1 && posy == n - 1) {
+			for (int i = 0; i < n; i++) {
+				for (int l = 0; l < n; l++) {
+					System.out.print(a[i][l] + " ");
 				}
 				System.out.println();
 			}
 			
-		} else if (posy==n-1) {
-			for (int l=0; l<n; l++) {
-				for (int i=n-1; i>=0; i--) {
-					System.out.print(a[i][l]+" ");
+		} else if (posx == n - 1) {
+			for (int l = n - 1; l >= 0; l--) {
+				for (int i = 0; i < n; i++) {
+					System.out.print(a[i][l] + " ");
+				}
+				System.out.println();
+			}
+			
+		} else if (posy == n - 1) {
+			for (int l = 0; l < n; l++) {
+				for (int i = n - 1; i >= 0; i--) {
+					System.out.print(a[i][l] + " ");
 				}
 				System.out.println();
 			}
 			
 			
 		} else {
-			for (int i=n-1; i>=0; i--) {
-				for (int l=n-1; l>=0; l--) {
-					System.out.print(a[i][l]+" ");
+			for (int i = n - 1; i >= 0; i--) {
+				for (int l=n - 1; l >= 0; l--) {
+					System.out.print(a[i][l] + " ");
 				}
 				System.out.println();
 			}
