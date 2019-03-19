a92522785cae1848561b112cf4b2a45338a05dd1
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-03-19 00:48:01 -0400

    Update ccc17j2.java

--- CCC Junior/ccc17j2.java
+++ CCC Junior/ccc17j2.java
@@ -4,15 +4,14 @@ public class ccc17j2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
 		Scanner sc = new Scanner(System.in);
 		
 		int k = sc.nextInt();
 		int n = sc.nextInt();
 		int v = k;
 		
-		for (int i=0; i<n; i++){
-			v+=Math.pow(10, i+1)*k;
+		for (int i = 0; i < n; i++){
+			v += Math.pow(10, i + 1) * k;
 		}
 		
 		System.out.println(v);
