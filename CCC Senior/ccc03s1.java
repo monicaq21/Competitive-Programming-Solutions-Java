076fc85685d3e8707da6ca565fa89f6d4b6f2080
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-03-20 01:20:54 -0400

    Update ccc03s1.java

--- CCC Senior/ccc03s1.java
+++ CCC Senior/ccc03s1.java
@@ -8,38 +8,35 @@ public class ccc03s1 {
     
 		int p = 1;
 		
-		while (true){
+		while (true) {
 			int k = sc.nextInt();
-			if (k>12 || k<2){
-				System.out.println("You Quit!"); break;
+			
+			if (k > 12 || k < 2){
+				System.out.println("You Quit!"); 
+				break;
 			}
+			
 			p += k;
-			if (p>100){
+			if (p > 100){
 				p -= k;
-			}
-			if (p==100){
+			} else if (p == 100){
 				System.out.println("You are now on square 100");
-				System.out.println("You Win!"); break;
+				System.out.println("You Win!"); 
+				break;
+			} else if (p == 54){
+				p = 19;
+			} else if (p == 90){
+				p = 48;
+			} else if (p == 99){
+				p = 77;
+			} else if (p == 9){
+				p = 34;
+			} else if (p == 40){
+				p = 64;
+			} else if (p == 67){
+				p = 86;
 			}
-			if (p==54){
-				p=19;
-			}
-			if (p==90){
-				p=48;
-			}
-			if (p==99){
-				p=77;
-			}
-			if (p==9){
-				p=34;
-			}
-			if (p==40){
-				p=64;
-			}
-			if (p==67){
-				p=86;
-			}
-			System.out.println("You are now on square "+p);
+			System.out.println("You are now on square " + p);
 		}
 	}
 
