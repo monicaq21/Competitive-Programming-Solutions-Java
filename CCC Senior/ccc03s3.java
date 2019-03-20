ce93a6c000fa0a88c4f1d97b98952ae869594a96
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-03-20 01:21:49 -0400

    Update ccc03s3.java

--- CCC Senior/ccc03s3.java
+++ CCC Senior/ccc03s3.java
@@ -1,5 +1,3 @@
-package contest_questions;
-
 import java.util.*;
 
 public class ccc03s3 {
@@ -15,37 +13,40 @@ public class ccc03s3 {
 		char[][] map = new char[r][c];
 		boolean[][] visited = new boolean[r][c];
 		LinkedList<Integer> area = new LinkedList<Integer>();
-		for (int i=0; i<r; i++){
+		
+		for (int i = 0; i < r; i++){
 			String s = sc.next();
 			map[i] = s.toCharArray();
 		}
 		
-		for (int i=0; i<r; i++){
-			for (int l=0; l<c; l++){
-				if (map[i][l]=='.'&&!visited[i][l]){
-					area.add(dfs(0,map,visited,i,l,r,c));
+		for (int i = 0; i < r; i++){
+			for (int l = 0; l < c; l++){
+				if (map[i][l] == '.' && !visited[i][l]){
+					area.add(dfs(0, map, visited, i, l, r, c));
 				}
 			}
 		}
+		
 		Collections.sort(area);
-		int j = area.size()-1;
+		int j = area.size() - 1;
 		int count = 0;
-		while (true){
-			if (j<0){
-				if (count==1){
-					System.out.println("1 room, "+wood+" square metre(s) left over");
+		
+		while (true) {
+			if (j < 0){
+				if (count == 1){
+					System.out.println("1 room, " + wood + " square metre(s) left over");
 					break;
 				}
-				System.out.println(count+" rooms, "+wood+" square metre(s) left over");
+				System.out.println(count + " rooms, " + wood + " square metre(s) left over");
 				break;
 			}
-			int left = wood-area.get(j);
-			if (left<0){
-				if (count==1){
-					System.out.println("1 room, "+wood+" square metre(s) left over");
+			int left = wood - area.get(j);
+			if (left < 0){
+				if (count == 1){
+					System.out.println("1 room, " + wood + " square metre(s) left over");
 					break;
 				}
-				System.out.println(count+" rooms, "+wood+" square metre(s) left over");
+				System.out.println(count + " rooms, " + wood + " square metre(s) left over");
 				break;
 			}
 			count++;
@@ -54,15 +55,16 @@ public class ccc03s3 {
 		}
 		
 	}
-	public static int dfs(int count,char[][]map,boolean[][]visited,int i,int l,int r,int c){
-		if (i<0||l<0||i>=r||l>=c||visited[i][l]||map[i][l]!='.'){
+	
+	public static int dfs(int count, char[][] map, boolean[][] visited, int i, int l, int r, int c){
+		if (i < 0 || l < 0 || i >= r || l >= c || visited[i][l] || map[i][l] != '.'){
 			return 0;
 		} else {
 			visited[i][l] = true;
-			return dfs(count,map,visited,i+1,l,r,c) +
-			dfs(count,map,visited,i-1,l,r,c) +
-			dfs(count,map,visited,i,l+1,r,c) +
-			dfs(count,map,visited,i,l-1,r,c) +
+			return dfs(count, map, visited, i + 1, l, r, c) +
+			dfs(count, map, visited, i - 1, l, r, c) +
+			dfs(count, map, visited, i, l + 1, r, c) +
+			dfs(count, map, visited, i, l - 1, r, c) +
 			1;
 		}
 	}
