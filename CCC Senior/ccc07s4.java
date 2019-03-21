fd0fde85c8f9d7cc35a28b2a1a474a39c9b959af
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-03-21 01:02:55 -0400

    Update ccc07s4.java

--- CCC Senior/ccc07s4.java
+++ CCC Senior/ccc07s4.java
@@ -9,30 +9,31 @@ public class ccc07s4 {
 		int v = sc.nextInt();
 		ArrayList<Integer>[] map = new ArrayList[v];
 		
-	    	for (int i=0; i<map.length; i++){
+	    	for (int i = 0; i < map.length; i++){
 	    		map[i] = new ArrayList<Integer>();
 	   	}
 		
 		while (true){
-			int r = sc.nextInt()-1;
-			int c = sc.nextInt()-1;
-			if (r==-1&&c==-1){
-				break;
-			}
+			int r = sc.nextInt() - 1;
+			int c = sc.nextInt() - 1;
+			
+			if (r == -1 && c == -1) break;
+			
 			map[r].add(c);
 			map[c].add(r);
 		}
 		
-		int[] ways = new int[v+1];
+		int[] ways = new int[v + 1];
 		ways[0] = 1;
-		for (int i=0; i<v; i++){
-			for (int l=i+1; l<v; l++){
+		
+		for (int i = 0; i < v; i++){
+			for (int l = i + 1; l < v; l++){
 				if (map[i].contains(l)){
 					ways[l] += ways[i];
 				}
 			}
 		}
-		System.out.println(ways[v-1]);
+		System.out.println(ways[v - 1]);
 		
 	}
 
