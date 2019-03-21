2544c18cda974b272680d16893d581228a14b65a
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-03-21 01:37:14 -0400

    Update ccc07s5.java

--- CCC Senior/ccc07s5.java
+++ CCC Senior/ccc07s5.java
@@ -7,14 +7,14 @@ public class ccc07s5 {
 	static boolean[][] been;
 	
 	public static int dfs(int balls, int start, int[] sum, int length, int n) {
-		if (balls==0) return 0;
-		if (start>=n) return 0;
+		if (balls == 0) return 0;
+		if (start >= n) return 0;
 		if (been[balls][start]) return dp[balls][start];
 		
-		int newScore = sum[Math.min(start+length-1, n-1)];
-		if (start>0) newScore -= sum[start-1];
-		dp[balls][start] = Math.max(dfs(balls-1,start+length,sum,length,n) + newScore, 
-				dfs(balls,start+1,sum,length,n));
+		int newScore = sum[Math.min(start + length - 1, n - 1)];
+		if (start > 0) newScore -= sum[start - 1];
+		dp[balls][start] = Math.max(dfs(balls - 1, start + length, sum, length, n) + newScore, 
+				dfs(balls, start + 1, sum, length, n));
 		been[balls][start] = true;
 		return dp[balls][start];
 
@@ -25,18 +25,18 @@ public class ccc07s5 {
 
 		int t = sc.nextInt();
 
-		for (int T=0; T<t; T++) {
+		for (int T = 0; T < t; T++) {
 			int n = sc.nextInt(), k = sc.nextInt(), w = sc.nextInt();
 			int[] a = new int[n], sum = new int[n];
 			sum[0] = a[0] = sc.nextInt();
-			dp = new int[k+1][n+1];
-			been = new boolean[k+1][n+1];
+			dp = new int[k + 1][n + 1];
+			been = new boolean[k + 1][n + 1];
 
-			for (int i=1; i<n; i++) {
+			for (int i = 1; i < n; i++) {
 				a[i] = sc.nextInt();
-				sum[i] = sum[i-1]+a[i];
+				sum[i] = sum[i - 1] + a[i];
 			}
-			System.out.println(dfs(k,0,sum,w,n));
+			System.out.println(dfs(k, 0, sum, w, n));
 		}
 
 
