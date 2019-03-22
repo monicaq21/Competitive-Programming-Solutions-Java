bda817f5f8cc700009f5a833d12a2a96126d5d42
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-03-21 21:37:33 -0400

    Update ccc09s3.java

--- CCC Senior/ccc09s3.java
+++ CCC Senior/ccc09s3.java
@@ -16,25 +16,24 @@ public class ccc09s3 {
 		map[3][5]=true;	map[5][3]=true;
 		map[3][4]=true;	map[4][3]=true;
 		map[4][5]=true;	map[5][4]=true;
-		map[3][15]=true;	map[15][3]=true;
-		map[15][13]=true;	map[13][15]=true;
-		map[12][13]=true;	map[13][12]=true;
-		map[13][14]=true;	map[14][13]=true;
-		map[18][16]=true;	map[16][18]=true;
-		map[17][16]=true;	map[16][17]=true;
-		map[17][18]=true;	map[18][17]=true;
-		map[12][11]=true;	map[11][12]=true;
-		map[12][9]=true;	map[9][12]=true;
-		map[11][10]=true;	map[10][11]=true;
-		map[10][9]=true;	map[9][10]=true;
+		map[3][15]=true; map[15][3]=true;
+		map[15][13]=true; map[13][15]=true;
+		map[12][13]=true; map[13][12]=true;
+		map[13][14]=true; map[14][13]=true;
+		map[18][16]=true; map[16][18]=true;
+		map[17][16]=true; map[16][17]=true;
+		map[17][18]=true; map[18][17]=true;
+		map[12][11]=true; map[11][12]=true;
+		map[12][9]=true; map[9][12]=true;
+		map[11][10]=true; map[10][11]=true;
+		map[10][9]=true; map[9][10]=true;
 		map[9][8]=true;	map[8][9]=true;
 		map[7][8]=true;	map[8][7]=true;
 
 		while (true){
 			String s = sc.nextLine();
-			if (s.equals("q")){
-				break;
-			}
+			if (s.equals("q")) break;
+			
 			if (s.equals("i")){
 				int x = sc.nextInt();
 				int y = sc.nextInt();
@@ -54,7 +53,8 @@ public class ccc09s3 {
 			if (s.equals("n")){
 				int x = sc.nextInt();
 				int count = 0;
-				for (int i=1; i<51; i++){
+				
+				for (int i = 1; i < 51; i++){
 					if (map[x][i]){
 						count++;
 					}
@@ -63,29 +63,29 @@ public class ccc09s3 {
 				continue;
 			}
 			if (s.equals("f")){
+				int x = sc.nextInt();
 				LinkedList<Integer> l = new LinkedList<Integer>();
 				LinkedList<Integer> l2 = new LinkedList<Integer>();
 				LinkedList<Integer> l3 = new LinkedList<Integer>();
-				int x = sc.nextInt();
-				for (int i=1; i<51; i++){
+				
+				for (int i = 1; i < 51; i++){
 					if (map[x][i]){
 						l.add(i);
 					}
 				}
-				for (int w=0; w<l.size(); w++){
+				for (int w = 0; w < l.size(); w++){
 					int n = l.get(w);
-					for (int i=1; i<51; i++){
-						if (map[n][i]&&i!=x&&!l.contains(i)){
-							l2.add(i); //2nd friends
+					for (int i = 1; i < 51; i++){
+						if (map[n][i] && i != x && !l.contains(i)){
+							l2.add(i);
 						}
 					}
 				}
-				for (int i=0; i<l2.size(); i++){
+				for (int i = 0; i < l2.size(); i++){
 					if (!l3.contains(l2.get(i))){
 						l3.add(l2.get(i));
 					}
 				}
-
 
 				System.out.println(l3.size());
 			}
@@ -93,17 +93,14 @@ public class ccc09s3 {
 			if (s.equals("s")){
 				int x = sc.nextInt();
 				int y = sc.nextInt();
-				LinkedList<Integer> queue=new LinkedList<Integer>();
+				LinkedList<Integer> queue = new LinkedList<Integer>();
 				queue.add(x);
 				int[] steps = new int[51];
 				Arrays.fill(steps, Integer.MAX_VALUE);
-				steps[x]=0;
-
+				steps[x] = 0;
 				
-				
-				
-				int r = p(map,queue,steps,y);
-				if (r==-1){
+				int r = p(map, queue, steps, y);
+				if (r == -1){
 					System.out.println("Not connected");
 				} else {
 					System.out.println(r);
@@ -116,16 +113,16 @@ public class ccc09s3 {
 
 
 	}
-	public static int p(boolean[][] map,LinkedList<Integer>queue,int[] steps, int y){
+	public static int p (boolean[][] map, LinkedList<Integer> queue, int[] steps, int y){
 		while (!queue.isEmpty()){
 			int n = queue.poll();
-			for (int i=1; i<51; i++){
-				if (map[n][i]&&steps[n]+1<steps[i]){
-					if (i==y){
-						return steps[n]+1;
-					}else {
+			for (int i = 1; i < 51; i++){
+				if (map[n][i] && steps[n] + 1 < steps[i]){
+					if (i == y){
+						return steps[n] + 1;
+					} else {
 						queue.add(i);
-						steps[i]=steps[n]+1;
+						steps[i] = steps[n] + 1;
 					}
 				}
 			}
