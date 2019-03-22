a17d0b95b5da1b0ba3be4afa9aeb33ed7fe91153
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-03-21 21:24:40 -0400

    Update ccc08s3.java

--- CCC Senior/ccc08s3.java
+++ CCC Senior/ccc08s3.java
@@ -2,7 +2,87 @@ import java.io.*;
 import java.util.*;
 
 public class ccc08s3 {
+	
+	static int[] parent;
 
+	public static class edge implements Comparable<edge> {
+		int bv, ev, cost, danger;
+		
+		public edge (int b, int e, int c, int d){
+			bv = b;ev = e;cost = c; danger = d;
+		}
+		@Override
+		public int compareTo (edge o) {
+			// TODO Auto-generated method stub
+			if (danger == o.danger){
+				return cost - o.cost;
+			} 
+			return danger - o.danger;
+		}
+	}
+	
+	public static int find (int i){
+		if (parent[i] == -1) return i;
+		else return find(parent[i]);
+	}
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		FastReader sc = new FastReader();
+
+		int t = sc.nextInt();
+		
+		for (int w = 0; w < t; w++){
+			int r = sc.nextInt(), c = sc.nextInt();
+			char[][] map = new char[r][c];
+			int[][] steps = new int[r][c];
+			LinkedList<Integer> X = new LinkedList<Integer>();
+			LinkedList<Integer> Y = new LinkedList<Integer>();
+			
+			for (int i = 0; i < r; i++){
+				String s = sc.next();
+				map[i] = s.toCharArray();
+			}
+			
+			for (int i = 0; i < r; i++){
+				Arrays.fill(steps[i], 1<<30);
+			}
+			
+			steps[0][0] = 1;
+			X.add(0); Y.add(0);
+			
+			while (!X.isEmpty()){
+				int x = X.poll();
+				int y = Y.poll();
+				
+				if (map[x][y] == '-' || map[x][y] == '+'){
+					if (y + 1 < c && map[x][y + 1] != '*' && steps[x][y] + 1 < steps[x][y + 1]){
+						steps[x][y + 1] = steps[x][y] + 1;
+						X.add(x); Y.add(y + 1);
+					} if (y - 1 >= 0 && map[x][y - 1] != '*' && steps[x][y] + 1 < steps[x][y - 1]){
+						steps[x][y - 1] = steps[x][y] + 1;
+						X.add(x); Y.add(y - 1);
+					}
+				}
+				
+				if (map[x][y] == '|' || map[x][y] == '+'){
+					if (x + 1 < r && map[x + 1][y] != '*' && steps[x][y] + 1 < steps[x + 1][y]){
+						steps[x + 1][y] = steps[x][y] + 1;
+						X.add(x + 1); Y.add(y);
+					} if (x - 1 >= 0 && map[x - 1][y] != '*' && steps[x][y] + 1 < steps[x - 1][y]){
+						steps[x - 1][y] = steps[x][y] + 1;
+						X.add(x - 1); Y.add(y);
+					}
+				}
+			}
+			
+			if (steps[r - 1][c - 1] == 1<<30) System.out.println(-1);
+			else System.out.println(steps[r - 1][c - 1]);
+			
+		}
+		
+	}
+	
 	static class FastReader
 	{
 		BufferedReader br;
@@ -53,80 +133,6 @@ public class ccc08s3 {
 			}
 			return str;
 		}
-	}
-
-	public static class edge implements Comparable<edge>{
-		int bv, ev, cost, danger;
-		public edge(int b,int e,int c,int d){
-			bv=b;ev=e;cost=c; danger=d;
-		}
-		@Override
-		public int compareTo(edge o) {
-			// TODO Auto-generated method stub
-			if (danger==o.danger){
-				return cost - o.cost;
-			} return danger - o.danger;
-		}
-	}
-	
-	static int[] parent;
-	
-	public static int find(int i){
-		if (parent[i]==-1) return i;
-		else return find(parent[i]);
-	}
-
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		FastReader sc = new FastReader();
-
-		int t = sc.nextInt();
-		for (int w=0; w<t; w++){
-			int r = sc.nextInt(), c = sc.nextInt();
-			char[][] map = new char[r][c];
-			for (int i=0; i<r; i++){
-				String s = sc.next();
-				map[i] = s.toCharArray();
-			}
-			
-			LinkedList<Integer> X = new LinkedList<Integer>();
-			LinkedList<Integer> Y = new LinkedList<Integer>();
-			
-			int[][] steps = new int[r][c];
-			for (int i=0; i<r; i++){
-				Arrays.fill(steps[i], 1<<30);
-			}
-			steps[0][0] = 1;
-			X.add(0); Y.add(0);
-			while (!X.isEmpty()){
-				int x = X.poll();
-				int y = Y.poll();
-				if (map[x][y]=='-'||map[x][y]=='+'){
-					if (y+1<c&&map[x][y+1]!='*'&&steps[x][y]+1<steps[x][y+1]){
-						steps[x][y+1] = steps[x][y]+1;
-						X.add(x); Y.add(y+1);
-					} if (y-1>=0&&map[x][y-1]!='*'&&steps[x][y]+1<steps[x][y-1]){
-						steps[x][y-1] = steps[x][y]+1;
-						X.add(x); Y.add(y-1);
-					}
-				}
-				if (map[x][y]=='|'||map[x][y]=='+'){
-					if (x+1<r&&map[x+1][y]!='*'&&steps[x][y]+1<steps[x+1][y]){
-						steps[x+1][y] = steps[x][y]+1;
-						X.add(x+1); Y.add(y);
-					} if (x-1>=0&&map[x-1][y]!='*'&&steps[x][y]+1<steps[x-1][y]){
-						steps[x-1][y] = steps[x][y]+1;
-						X.add(x-1); Y.add(y);
-					}
-				}
-			}
-			if (steps[r-1][c-1]==1<<30) System.out.println(-1);
-			else System.out.println(steps[r-1][c-1]);
-			
-			
-			
-		}
-		
 	}
 
 
