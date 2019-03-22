a62fe2163281c3e2cf9f39a9a6f6cef5ee0557e4
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-03-21 21:41:15 -0400

    Update ccc09s4.java

--- CCC Senior/ccc09s4.java
+++ CCC Senior/ccc09s4.java
@@ -11,10 +11,10 @@ public class ccc09s4 {
 		int T = Integer.parseInt(br.readLine());
 		int[][] trade = new int[N][N];
 		
-		for (int i=0; i<T; i++) {
+		for (int i = 0; i < T; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int bv = Integer.parseInt(st.nextToken())-1;
-			int ev = Integer.parseInt(st.nextToken())-1;
+			int bv = Integer.parseInt(st.nextToken()) - 1;
+			int ev = Integer.parseInt(st.nextToken()) - 1;
 			int cost = Integer.parseInt(st.nextToken());
 			trade[bv][ev] = cost;
 			trade[ev][bv] = cost;
@@ -22,56 +22,61 @@ public class ccc09s4 {
 
 		int K = Integer.parseInt(br.readLine());
 		int[] price = new int[N];
-		for (int i=0; i<K; i++) {
+		
+		for (int i = 0; i < K; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int n = Integer.parseInt(st.nextToken())-1;
+			int n = Integer.parseInt(st.nextToken()) - 1;
 			price[n] = Integer.parseInt(st.nextToken());
 		}
 
-		int D = Integer.parseInt(br.readLine())-1;
+		int D = Integer.parseInt(br.readLine()) - 1;
 
 		int[] step = new int[N];
 		for (int i=0; i<N; i++) {
 			step[i] = Integer.MAX_VALUE;
 		}
 		step[D] = 0;
+		
 		boolean[] vis = new boolean[N];
-		for (int i=0; i<N; i++) {
+		for (int i = 0; i < N; i++) {
 			int min = Integer.MAX_VALUE;
 			int index = -1;
-			for (int j=0; j<N; j++) {
-				if (min>step[j] && vis[j]==false) {
+			
+			for (int j = 0; j < N; j++) {
+				if (min > step[j] && vis[j] == false) {
 					min = step[j];
 					index = j;
 				}
 			}
 
 			vis[index] = true;
-			for (int j=0; j<N; j++) {
-				if (trade[index][j]!=0 && vis[j]==false &&
-						step[j]>step[index]+trade[index][j]) {
-					step[j]=step[index]+trade[index][j];
+			
+			for (int j = 0; j < N; j++) {
+				if (trade[index][j] != 0 && vis[j] == false &&
+						step[j] > step[index] + trade[index][j]) {
+					step[j] = step[index] + trade[index][j];
 				}
 			}
-
 		}
+		
 		int min = Integer.MAX_VALUE;
-		for (int i=0; i<N; i++) {
-			if (price[i]!=0 && step[i]+price[i]<min) {
-				min = step[i]+price[i];
+		for (int i = 0; i < N; i++) {
+			if (price[i] != 0 && step[i] + price[i] < min) {
+				min = step[i] + price[i];
 			}
 		}
+		
 		System.out.println(min);
 	}
 
-	public static class Node implements Comparable<Node>{
+	public static class Node implements Comparable<Node> {
 		int v, step;
-		public Node(int v, int step) {
+		public Node (int v, int step) {
 			this.v = v;
 			this.step = step;
 		}
 		@Override
-		public int compareTo(Node o) {
+		public int compareTo (Node o) {
 			// TODO Auto-generated method stub
 			return this.step-o.step;
 		}
