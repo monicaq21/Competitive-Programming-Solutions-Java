72d4b9509fc58c39c4a4098add83444ce988ed01
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-03-22 22:36:12 -0400

    Update ccc12s1.java

--- CCC Senior/ccc12s1.java
+++ CCC Senior/ccc12s1.java
@@ -7,28 +7,28 @@ public class ccc12s1 {
 		Scanner sc = new Scanner(System.in);
 		
 		int n = sc.nextInt();
-		if (n<4){
+		
+		if (n < 4){
 			System.out.println(0);
-		} else if (n==4){
+		} else if (n == 4){
 			System.out.println(1);
-		} else if (n==5){
+		} else if (n == 5){
 			System.out.println(5);
-		} else if (n==6){
+		} else if (n == 6){
 			System.out.println(15);
-		} else if (n==7){
+		} else if (n == 7){
 			System.out.println(20);
 		} else {
 			System.out.println(factorial(n));
 		}
 	}
-  
-  
+	
 	public static int factorial (int a){
 		int x = 1;
-		for (int i=a-1; i>=a-3; i--){
-			x*=i;
+		for (int i = a - 1; i >= a - 3; i--){
+			x *= i;
 		}
-		return x/6;
+		return x / 6;
 	}
 
 }
