3772a96cf152a39c7a6586d28132f771e8e52160
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-03-22 22:40:09 -0400

    Update ccc12s5.java

--- CCC Senior/ccc12s5.java
+++ CCC Senior/ccc12s5.java
@@ -14,25 +14,26 @@ public class ccc12s5 {
 		ways[0][0] = 1;
 		
 		int cage = sc.nextInt();
-		for (int i=0; i<cage; i++){
-			int x = sc.nextInt()-1;
-			int y = sc.nextInt()-1;
+		for (int i = 0; i < cage; i++){
+			int x = sc.nextInt() - 1;
+			int y = sc.nextInt() - 1;
 			cat[x][y] = true;
 		}
 		
-		for (int i=0; i<r; i++){
-			for (int l=0; l<c; l++){
-				if (i<r-1&&!cat[i+1][l]){
-					ways[i+1][l] += ways[i][l];
+		for (int i = 0; i < r; i++){
+			for (int l = 0; l < c; l++){
+				if (i < r - 1 && !cat[i + 1][l]){
+					ways[i + 1][l] += ways[i][l];
 				}
-				if (l<c-1&&!cat[i][l+1]){
-					ways[i][l+1] += ways[i][l];
+				
+				if (l < c - 1 && !cat[i][l + 1]){
+					ways[i][l + 1] += ways[i][l];
 				}
 			}
 		}
 		
 		
-		System.out.println(ways[r-1][c-1]);
+		System.out.println(ways[r - 1][c - 1]);
 		
 	}
 
