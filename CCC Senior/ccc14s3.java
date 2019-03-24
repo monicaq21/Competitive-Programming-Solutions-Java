8cdce99a50831bcde9ac05deeb16811f7deab411
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-03-23 23:55:14 -0400

    Update ccc14s3.java

--- CCC Senior/ccc14s3.java
+++ CCC Senior/ccc14s3.java
@@ -5,28 +5,32 @@ public class ccc14s3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
+		
 		int t = sc.nextInt();
 		
-		for (int w=0; w<t; w++){
-			int n = sc.nextInt();
-			Stack<Integer> branch = new Stack<Integer>(),
-					mountain = new Stack<Integer>();
-			int cur = 0;
-			for (int i=0; i<n; i++) {
-				int g = sc.nextInt()-1;
+		for (int w = 0; w < t; w++){
+			int n = sc.nextInt(), cur = 0;
+			Stack<Integer> branch = new Stack<Integer>();
+			Stack<Integer> mountain = new Stack<Integer>();
+
+			for (int i = 0; i < n; i++) {
+				int g = sc.nextInt() - 1;
 				mountain.push(g);
 			}
+			
 			while (true){
-				if (!branch.isEmpty()&&branch.peek()==cur){
-					cur++; branch.pop();
-				} else if (!mountain.isEmpty()&&mountain.peek()==cur){
-					cur++; mountain.pop();
+				if (!branch.isEmpty() && branch.peek() == cur){
+					cur++; 
+					branch.pop();
+				} else if (!mountain.isEmpty() && mountain.peek() == cur){
+					cur++; 
+					mountain.pop();
 				} else if (!mountain.isEmpty()){
 					int m = mountain.pop();
 					branch.push(m);
 				} else break;
 			}
-			if (cur==n) System.out.println("Y");
+			if (cur == n) System.out.println("Y");
 			else System.out.println("N");
 			
 		}
