e6608238aadf0d19b5d658410da3f8c442aa1f9c
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-03-24 00:06:17 -0400

    Update ccc16s2.java

--- CCC Senior/ccc16s2.java
+++ CCC Senior/ccc16s2.java
@@ -1,5 +1,4 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class ccc16s2 {
 
@@ -9,41 +8,40 @@ public class ccc16s2 {
 
 		int question = sc.nextInt();
 		int n = sc.nextInt();
+		
 		int[] a = new int[n];
 		int[] b = new int[n];
 		int c = 0;
 
-		for (int i=0; i<n; i++){
+		for (int i = 0; i < n; i++){
 			a[i] = sc.nextInt();
 		}
-		for (int i=0; i<n; i++){
+		for (int i = 0; i < n; i++){
 			b[i] = sc.nextInt();
 		}
 		Arrays.sort(a);
 		Arrays.sort(b);
 
-		if (question==1){
-			for (int i=0; i<n; i++){
-				if (a[i]>b[i]){
-					c =c+a[i];
+		if (question == 1){
+			for (int i = 0; i < n; i++){
+				if (a[i] > b[i]){
+					c = c + a[i];
 				} else {
-					c =c+b[i];
+					c = c + b[i];
 				}
 			}
 		} else {
-			for (int i=0; i<n; i++){
-				if (a[i]>b[n-1-i]){
-					c =c+a[i];
+			for (int i = 0; i < n; i++){
+				if (a[i] > b[n - 1 - i]){
+					c = c + a[i];
 				} else {
-					c =c+b[n-1-i];
+					c = c + b[n - 1 - i];
 				}
 			}
 		}
 
-		
 		System.out.println(c);
-
-
+		
 	}
 
 }
