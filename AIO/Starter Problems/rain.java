23be4f965e9cbd20fd0b2cf6ab5bf8fa9f59aefe
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-03-24 22:05:46 -0400

    Create rain.java

---
+++ AIO/Starter Problems/rain.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(new File("rainin.txt"));
+		PrintWriter pw = new PrintWriter(new File("rainout.txt"));
+		
+		int n = sc.nextInt(), c = sc.nextInt(), cur = 0, index = -1;
+		
+		for (int i = 0; i < n; i++) {
+			cur += sc.nextInt();
+      
+			if (index == -1 && cur >= c) {
+				index = i + 1;
+			}
+		}
+		
+		pw.println(index);
+		
+		pw.close();
+
+	}
+}
