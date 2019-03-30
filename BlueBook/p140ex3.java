6455906c66e827e6fcc50f71ffc8829973db942c
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-03-29 22:33:19 -0400

    Update p140ex3.java

--- BlueBook/p140ex3.java
+++ BlueBook/p140ex3.java
@@ -7,8 +7,9 @@ public class p140ex3 {
 		Scanner sc = new Scanner(System.in);
 
 		String n = sc.next();
-		for (int i=0; i<n.length(); i++){
-			System.out.println(n.charAt(n.length()-i-1));
+		
+		for (int i = 0; i < n.length(); i++){
+			System.out.println(n.charAt(n.length() - i - 1));
 		}
 		
 
