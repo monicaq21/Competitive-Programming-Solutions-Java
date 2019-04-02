ae9294f39eec9a32779f81f53a22d948cb82bfaa
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-04-02 08:39:53 -0400

    Update p175ex5.java

--- BlueBook/p175ex5.java
+++ BlueBook/p175ex5.java
@@ -8,13 +8,14 @@ public class p175ex5 {
 
 		int t = sc.nextInt();
 
-		for (int w=0; w<t; w++) {
+		for (int w = 0; w < t; w++) {
+			
 			int n = sc.nextInt();
 			int[][] table = new int[n][n];
 			int[] row = new int[n], col = new int[n];
 
-			for (int i=0; i<n; i++) {
-				for (int l=0; l<n; l++) {
+			for (int i = 0; i < n; i++) {
+				for (int l = 0; l < n; l++) {
 					table[i][l] = sc.nextInt();
 					row[i] += table[i][l];
 					col[l] += table[i][l];
