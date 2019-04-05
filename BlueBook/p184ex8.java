c64a26fe5c5066d7f646d94ce391cb9882ca9e95
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-04-05 19:11:09 -0400

    Update p184ex8.java

--- BlueBook/p184ex8.java
+++ BlueBook/p184ex8.java
@@ -9,11 +9,14 @@ public class p184ex8 {
 
 		int n = sc.nextInt();
 		int[] count = new int[7];
-		for (int i=0; i<n; i++) {
+		for (int i = 0; i < n; i++) {
+			
 			String s = sc.next();
-			int index = (int)(s.charAt(0))-65;
-			if (index>=6||index<0)  count[6]++;
+			int index = (int) (s.charAt(0)) - 65;
+			
+			if (index >= 6 || index < 0)  count[6]++;
 			else count[index]++;
+
 		}
 		
 		for (int i: count) System.out.println(i);
