0c3934931a6a0636fd3cb7e0da07f5cc0ef36d06
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-04-05 19:10:23 -0400

    Update p183ex7.java

--- BlueBook/p183ex7.java
+++ BlueBook/p183ex7.java
@@ -4,8 +4,8 @@ import java.io.*;
 public class p183ex7 {
 	
 	public static boolean isPrime(int n) {
-		for (int i=2; i<=(int)Math.ceil(Math.sqrt(n)); i++) {
-			if (n%i==0) return false;
+		for (int i = 2; i <= (int) Math.ceil(Math.sqrt(n)); i++) {
+			if (n % i == 0) return false;
 		}
 		return true;
 	}
@@ -17,14 +17,16 @@ public class p183ex7 {
 
 		int n = sc.nextInt();
 		
-		if (n==1) System.out.println(0);
-		else {
-			System.out.println(0+"\n"+1);
+		if (n == 1) {
+			System.out.println(0);
+		} else {
+			System.out.println(0 + "\n" + 1);
 			
-			for (int i=3; i<=n; i++) {
+			for (int i = 3; i <= n; i++) {
 				if (isPrime(i)) System.out.println(1);
 				else System.out.println(0);
 			}
+			
 		}
 		
 		
