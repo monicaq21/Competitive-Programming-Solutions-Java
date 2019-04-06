c8674e072692b01ea9f80e95b3e636ac64f30348
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-04-06 19:55:23 -0400

    Update p307ex9.java

--- BlueBook/p307ex9.java
+++ BlueBook/p307ex9.java
@@ -7,15 +7,19 @@ public class p307ex9 {
 		Scanner sc =  new Scanner(System.in);
 
 		int a = sc.nextInt();
-		for (int i = 0; i<a; i++){
+		
+		for (int i = 0; i < a; i++){
+			
 			int b = sc.nextInt();
-			if (b%400==0){
+			
+			if (b % 400 == 0){
 				System.out.println("1");
-			} else if (b%4==0 && b%100!=0){
+			} else if (b % 4 == 0 && b % 100 != 0){
 				System.out.println("1");
 			} else {
 				System.out.println("0");
 			}
+			
 		}
 	}
 
