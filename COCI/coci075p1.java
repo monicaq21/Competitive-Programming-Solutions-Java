dfa45e9bc28d0f08c48c7d4e6d149e948af7040a
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-04-10 20:47:15 -0400

    Update coci075p1.java

--- COCI/coci075p1.java
+++ COCI/coci075p1.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 public class coci075p1 {
 
-
 	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
@@ -11,22 +10,22 @@ public class coci075p1 {
 		int b = sc.nextInt();
 		int c = sc.nextInt();
 		
-		if (a+b==c){
-			System.out.println(a+"+"+b+"="+c);
-		} else if (a==b+c){
-			System.out.println(a+"="+b+"+"+c);
-		} else if (a==b-c){
-			System.out.println(a+"="+b+"-"+c);
-		} else if (a*b==c){
-			System.out.println(a+"*"+b+"="+c);
-		} else if (a==b*c){
-			System.out.println(a+"="+b+"*"+c);
-		} else if (a-b==c){
-			System.out.println(a+"-"+b+"="+c);
-		} else if (a/b==c){
-			System.out.println(a+"/"+b+"="+c);
-		} else if (a==b/c){
-			System.out.println(a+"="+b+"/"+c);
+		if (a + b == c){
+			System.out.println(a + "+" + b + "=" + c);
+		} else if (a == b + c){
+			System.out.println(a + "=" + b + "+" + c);
+		} else if (a == b - c){
+			System.out.println(a + "=" + b + "-" + c);
+		} else if (a * b == c){
+			System.out.println(a + "*" + b + "=" + c);
+		} else if (a == b * c){
+			System.out.println(a + "=" + b + "*" + c);
+		} else if (a - b == c){
+			System.out.println(a + "-" + b + "=" + c);
+		} else if (a / b == c){
+			System.out.println(a + "/" + b + "=" + c);
+		} else if (a == b / c){
+			System.out.println(a + "=" + b + "/" + c);
 		}
 		
 		
