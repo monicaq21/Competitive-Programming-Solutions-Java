71bc346a3198160668f362cf2418f1c20025783f
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-04-12 08:09:01 -0400

    Update coci073p1.java

--- COCI/coci073p1.java
+++ COCI/coci073p1.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 public class coci073p1 {
 
-
 	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
