c3eeab6e14c610550611a165457c41e2358d44af
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-04-13 00:23:45 -0400

    Update coci072p3.java

--- COCI/coci072p3.java
+++ COCI/coci072p3.java
@@ -5,56 +5,67 @@ public class coci072p3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-
-		LinkedList<String> l = new LinkedList<String>();
-
+		
 		int r = sc.nextInt();
 		int c = sc.nextInt();
+		
 		char[][] k = new char[r][c];
+		LinkedList<String> l = new LinkedList<String>();
 
-		for (int i=0; i<r; i++){
+		for (int i = 0; i < r; i++){
 			String s = sc.next();
-			k[i]=s.toCharArray();
+			k[i] = s.toCharArray();
 		}
 
-		for (int i=0; i<r; i++){
+		for (int i = 0; i < r; i++){
+			
 			String s = "";
-			for (int q=0; q<c; q++){
-				if (k[i][q]!='#'){
-					s+=k[i][q]+"";
+			
+			for (int q = 0; q < c; q++){
+				
+				if (k[i][q] != '#'){
+					s += k[i][q] + "";
 				} else {
-					if (s.length()>1){
+					if (s.length() > 1){
 						l.add(s);
 					}
-					s="";
+					s = "";
 				}
+				
 			}
 			
-			if (s.length()>1){
+			if (s.length() > 1){
 				l.add(s);
 			}
 		}
 		
 		
 		
-		for (int q=0; q<c; q++){
+		for (int q = 0; q < c; q++){
+			
 			String s = "";
-			for (int i=0; i<r; i++){
-				if (k[i][q]!='#'){
-					s+=k[i][q]+"";
+			
+			for (int i = 0; i < r; i++){
+				
+				if (k[i][q] != '#'){
+					s += k[i][q] + "";
 				} else {
-					if (s.length()>1){
+					if (s.length() > 1){
 						l.add(s);
 					}
-					s="";
+					
+					s = "";
 				}
+				
 			}
 			
-			if (s.length()>1){
+			if (s.length() > 1){
 				l.add(s);
 			}
 		}
+		
 		Collections.sort(l);
 		System.out.println(l.peek());
+		
 	}
 }
