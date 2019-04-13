1aeac0e3b45ebd07d1a79eaf09c0c4197af28758
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-04-13 00:24:37 -0400

    Update coci072p2.java

--- COCI/coci072p2.java
+++ COCI/coci072p2.java
@@ -1,19 +1,17 @@
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Scanner;
+import java.util.*;
 
 public class coci072p2 {
-
 
 	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
+		
 		int a = sc.nextInt();
 		
-		if (a%2==0){
-			System.out.println((a/2+1)*(a/2+1));
+		if (a % 2 == 0){
+			System.out.println((a / 2 + 1) * (a / 2 + 1));
 		} else{
-			System.out.println(((a-1)/2+1)*((a-1)/2+1)+((a-1)/2+1));
+			System.out.println(((a - 1) / 2 + 1) * ((a - 1) / 2 + 1) + ((a - 1) / 2 + 1));
 
 		}
 		
