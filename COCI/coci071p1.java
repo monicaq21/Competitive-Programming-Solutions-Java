b31acfe294a8ad83887363e7dfbb0f9402ec74b3
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-04-14 09:22:52 -0400

    Update coci071p1.java

--- COCI/coci071p1.java
+++ COCI/coci071p1.java
@@ -12,28 +12,28 @@ public class coci071p1 {
 		int y2 = sc.nextInt();
 		int x3 = sc.nextInt();
 		int y3 = sc.nextInt();
+		
 		int x = 0;
 		int y = 0;
     
-		if (x1==x2){
-			x=x3;
-		} else if (x1==x3){
-			x=x2;
+		if (x1 == x2){
+			x = x3;
+		} else if (x1 == x3){
+			x = x2;
 		} else {
-			x=x1;
+			x = x1;
 		}
     
     
-		if (y1==y2){
-			y=y3;
-		} else if (y1==y3){
-			y=y2;
+		if (y1 == y2){
+			y = y3;
+		} else if (y1 == y3){
+			y = y2;
 		} else {
-			y=y1;
+			y = y1;
 		}
     
-    
-		System.out.println(x+" "+y);
+		System.out.println(x + " " + y);
 		
 	}
 
