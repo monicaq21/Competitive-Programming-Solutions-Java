51a24d5e124e5ab8472ff65ad89a6310e5e838c9
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-04-16 22:55:53 -0400

    Update coci066p1.java

--- COCI/coci066p1.java
+++ COCI/coci066p1.java
@@ -6,22 +6,26 @@ public class coci066p1 {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 
-		int n = sc.nextInt();
-		int warnings = 0;
+		int n = sc.nextInt(), warnings = 0;
+		
 		LinkedList<String> name = new LinkedList<String>();
 		LinkedList<Integer> times = new LinkedList<Integer>();
     
-		for (int i=0; i<n; i++) {
+		for (int i = 0; i < n; i++) {
+			
 			String s = sc.next();
+			
 			if (!name.contains(s)) {
 				name.add(s);
 				times.add(1);
 			} else {
 				int index = name.indexOf(s);
-				if (times.get(index)>(i-times.get(index))){
+				
+				if (times.get(index) > (i - times.get(index))){
 					warnings++;
 				}
-				times.set(index, times.get(index)+1);
+				
+				times.set(index, times.get(index) + 1);
 				
 			}
 		}
