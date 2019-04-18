e0f6dc64b9cdb3dc0eb32a818df897b7096e4591
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-04-18 11:15:43 -0400

    Update coci063p2.java

--- COCI/coci063p2.java
+++ COCI/coci063p2.java
@@ -6,8 +6,8 @@ public class coci063p2 {
 		int x;
 		int y;
 		public pair(int i, int j) {
-			x=i;
-			y=j;
+			x = i;
+			y = j;
 		}
 	}
 	
@@ -15,33 +15,36 @@ public class coci063p2 {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 
-		Hashtable<Character,pair> h = new Hashtable<Character,pair>();
+		Hashtable <Character,pair> h = new Hashtable <Character,pair>();
     
 		for (int i=0; i<4; i++){
 			for (int l=0; l<4; l++){
       
-				if (i==3&&l==3){
+				if (i == 3 && l == 3){
 					break;
 				}
         
-				h.put((char)(i*4+'A'+l), new pair(i,l));
+				h.put((char) (i * 4 + 'A' + l), new pair(i, l));
         
 			}
 		}
 		
-    
 		int count = 0;
+		
 		for (int i=0; i<4; i++){
+			
 			String s = sc.nextLine();
       
 			for (int l=0; l<4; l++){
+				
 				char k = s.charAt(l);
         
-				if (k=='.') continue;
+				if (k == '.') continue;
         
 				pair e = h.get(k);
-				count+=Math.abs(e.x-i);
-				count+=Math.abs(e.y-l);
+				count += Math.abs(e.x - i);
+				count += Math.abs(e.y - l);
+				
 			}
       
 		}
