428836d017d6ce1e1ab90b631437c4e460f3abb3
Monica Qiu <44070924+monicaq21@users.noreply.github.com>
2019-04-26 14:08:19 -0400

    Update wc152s1.java

--- Woburn Challenge Junior/wc152s1.java
+++ Woburn Challenge Junior/wc152s1.java
@@ -8,24 +8,25 @@ public class wc152s1 {
 
 		int n = sc.nextInt();
 		int[] a = new int[n];
+		
 		for (int i=0; i<n; i++) a[i] = sc.nextInt();
+		
 		for (int i=0; i<n; i++){
-			if (a[i]>0) continue;
+			if (a[i] > 0) continue;
 			a[i] = 1;
-			if (i==0) {
-				if (a[i+1]==a[i]) a[i]=2;
-			}
-			else if (i==n-1) {
-				if (a[i-1]==a[i]) a[i]=2;
-			}
-			else {
+			if (i == 0) {
+				if (a[i+1] == a[i]) a[i] = 2;
+			} else if (i == n - 1) {
+				if (a[i-1] == a[i]) a[i] = 2;
+			} else {
 				while (true){
-					if (a[i]==a[i-1]||a[i]==a[i+1]){
+					if (a[i] == a[i-1] || a[i] == a[i+1]){
 						a[i]++;
 					} else break;
 				}
 			}
 		}
+		
 		for (int i=0; i<n; i++) System.out.print(a[i]);
 		
 	}
